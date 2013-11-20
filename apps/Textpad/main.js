36260ef1eb78ab321d232f0d43765b7715f17f73
Anders Evenrud <andersevenrud@gmail.com>
2013-11-21 00:25:22 +0100

    Updated apps metadata (2)

--- apps/Textpad/main.js
+++ apps/Textpad/main.js
@@ -74,7 +74,7 @@
     Window.prototype._onDndAction.apply(this, arguments);
     if ( type === 'itemDrop' && item ) {
       var data = item.data;
-      if ( data && data.type === 'file' && data.mime && data.mime.match(/^text\//) ) {
+      if ( data && data.type === 'file' && data.mime && (data.mime.match(/^text\//) || data.mime == 'application/x-empty') ) {
         this._appRef.action('open', data.path);
       }
     }
