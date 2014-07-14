b03285362179729e596db59bda4c63399bc59b8c
Anders Evenrud <andersevenrud@gmail.com>
2014-07-14 20:46:18 +0200

    Compability: iOS removed default scroll effect/bounce (disabled)

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -644,24 +644,6 @@
       self._onScroll.apply(self, arguments);
     }, false);
 
-    // FIXME: Destroy these
-    if ( OSjs.Utils.getCompability().touch ) {
-      var xStart, yStart = 0;
-
-      document.addEventListener('touchstart',function(e) {
-        xStart = e.touches[0].screenX;
-        yStart = e.touches[0].screenY;
-      });
-
-      document.addEventListener('touchmove',function(e) {
-        var xMovement = Math.abs(e.touches[0].screenX - xStart);
-        var yMovement = Math.abs(e.touches[0].screenY - yStart);
-        if((yMovement * 3) > xMovement) {
-          e.preventDefault();
-        }
-      });
-    }
-
 
     document.addEventListener('mouseout', function(ev) {
       self._onLeave(ev);
