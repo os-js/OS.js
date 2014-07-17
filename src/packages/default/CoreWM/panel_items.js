2d2dd655236c15a51e70ce5614b01a4629b1f9c8
Anders Evenrud <andersevenrud@gmail.com>
2014-07-17 19:45:43 +0200

    CoreWM: PanelItemNotificationArea CSS improvments

--- src/packages/default/CoreWM/panel_items.js
+++ src/packages/default/CoreWM/panel_items.js
@@ -349,6 +349,8 @@
   PanelItemNotificationArea.prototype.init = function() {
     var root = PanelItem.prototype.init.apply(this, arguments);
 
+    this.createNotification("FOOBAR", {className: "BusyNotification"})
+
     return root;
   };
 
