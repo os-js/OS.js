bae1255f4eeac1d67e5b8f7cf04105d3aaf9547a
Anders Evenrud <andersevenrud@gmail.com>
2014-08-07 00:04:00 +0200

    Version bump
    * Code syntax fix for compressed/combined dist
    * New DefaultApplication class
    * New DefaultApplicationWindow class
    * Fixed Window loadig indicator position
    * Fixed Window maximization
    * Updated locales
    * Application - Calculator: Cross-browser fixes
    * Fixed IE CSS loading for WM (seriously IE....seriously?!)

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -55,7 +55,7 @@
   OSjs.Locale       = OSjs.Locale       || {};
   OSjs.Core         = {};
   OSjs.API          = {};
-  OSjs.Version      = '2.0-alpha12';
+  OSjs.Version      = '2.0-alpha14';
 
   /////////////////////////////////////////////////////////////////////////////
   // INTERNAL VARIABLES
