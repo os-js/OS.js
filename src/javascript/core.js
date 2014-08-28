0a8dfad4a1b1f1cf0bda485f4ebb1f42d83ec3af
Anders Evenrud <andersevenrud@gmail.com>
2014-08-28 16:45:13 +0200

    Version bump

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -55,7 +55,7 @@
   OSjs.Locale       = OSjs.Locale       || {};
   OSjs.Core         = {};
   OSjs.API          = {};
-  OSjs.Version      = '2.0-alpha15';
+  OSjs.Version      = '2.0-alpha16';
 
   /////////////////////////////////////////////////////////////////////////////
   // INTERNAL VARIABLES
