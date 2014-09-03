57e2e564e3decdd08b37b023c03d034dfd5cbed6
Anders Evenrud <andersevenrud@gmail.com>
2014-09-03 18:31:57 +0200

    Server-PHP: Bugfix after MIME changes

--- src/server-php/vfs.php
+++ src/server-php/vfs.php
@@ -41,7 +41,7 @@ class MIME
     $file = sprintf("%s/src/%s", ROOTDIR, "mime.json");
     if ( file_exists($file) ) {
       $arr = (array)json_decode(file_get_contents($file));
-      $this->data = $arr["mapping"];
+      $this->data = (array)$arr["mapping"];
     }
   }
 
