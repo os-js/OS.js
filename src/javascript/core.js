20b7dc305ba2fc7162baf3082576966900661006
Anders Evenrud <andersevenrud@gmail.com>
2014-09-28 00:18:51 +0200

    Version bump
    * Added automated installer
    * Updated INSTALL instructions entirely
    * Core: Refactored to use forEach() loops instead of the traditional way
    * GUI: FileView bugfixes for iconview and treeview
    * Dialogs: File dialog bugfixes relating to GUIFileView

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -42,7 +42,7 @@
   OSjs.Hooks        = {};
   OSjs.Core         = {};
   OSjs.API          = {};
-  OSjs.Version      = '2.0-alpha21';
+  OSjs.Version      = '2.0-alpha22';
 
   /////////////////////////////////////////////////////////////////////////////
   // DEFAULT HOOKS
