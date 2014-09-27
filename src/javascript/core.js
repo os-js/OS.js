a9dffd4498980b1c8e93fce88accce622c92034b
Anders Evenrud <andersevenrud@gmail.com>
2014-09-28 01:44:06 +0200

    Version bump
    * Version stamp now created on startup
    * Application - Draw: Now uses extended namespaces
    * Application - CoreWM: Now uses extended namespaces
    * Application - FileManager: Sidebar bugfix after previous bump
    * GUI - FileView: Fixed filesize sorting

--- src/javascript/core.js
+++ src/javascript/core.js
@@ -42,7 +42,7 @@
   OSjs.Hooks        = {};
   OSjs.Core         = {};
   OSjs.API          = {};
-  OSjs.Version      = '2.0-alpha22';
+  OSjs.Version      = '2.0-alpha23';
 
   /////////////////////////////////////////////////////////////////////////////
   // DEFAULT HOOKS
