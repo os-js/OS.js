fd77d3c570c3d20286ef68b4cc1930fe13161536
Anders Evenrud <andersevenrud@gmail.com>
2014-12-29 21:28:13 +0100

    Minor cleanup

--- src/server-php/vfs.php
+++ src/server-php/vfs.php
@@ -169,8 +169,6 @@ class FS
     } else {
       $content = base64_decode($content);
     }
-    //$content = iconv("ISO-8859-1","UTF-8", $content);
-
 
     return file_put_contents($fname, $content) !== false;
   }
