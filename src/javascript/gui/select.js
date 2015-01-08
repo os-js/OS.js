3b49a581680f1c4a35489002e020d4d848e034fb
Ryan Riffle <ryanriffle@icloud.com>
2015-01-08 11:41:58 -0500

    Fixes toggleable toolbar item not actually looking toggled. Added a return value to OSjs.GUI.Select.setSelected() and .setValue()

--- src/javascript/gui/select.js
+++ src/javascript/gui/select.js
@@ -3,16 +3,16 @@
  *
  * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -64,7 +64,7 @@
   };
 
   Select.prototype.setValue = function(val) {
-    this.setSelected(val);
+    return this.setSelected(val);
   };
 
   Select.prototype.setSelected = function(val) {
@@ -81,7 +81,10 @@
     if ( found !== false ) {
       this.$input.selectedIndex = found;
       this.value = found;
+      return true;
     }
+
+    return false;
   };
 
   /////////////////////////////////////////////////////////////////////////////
