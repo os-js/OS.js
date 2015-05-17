792a7057fd93459e832960eaaa6d0532fea44a74
Anders Evenrud <andersevenrud@gmail.com>
2015-05-17 14:59:56 +0200

    Added windows installer script (#94)

---
+++ src/tools/installer.cmd
@@ -0,0 +1,33 @@
+REM Fixing Symlinks for OS.js dist-dev
+REM Run this as an administrator from OS.js root directory
+REM EX: src\tools\installer.cmd
+
+SET srcdir=%CD%
+SET dstdir=%CD%\dist-dev
+
+del %dstdir%\blank.css
+mklink "%dstdir%\blank.css" "%srcdir%\dist\blank.css"
+
+del %dstdir%\favicon.ico
+mklink "%dstdir%\favicon.ico" "%srcdir%\dist\favicon.ico"
+
+del %dstdir%\favicon.png
+mklink "%dstdir%\favicon.png" "%srcdir%\dist\favicon.png"
+
+del %dstdir%\settings.js
+mklink "%dstdir%\settings.js" "%srcdir%\dist\settings.js"
+
+del %dstdir%\css
+mklink /j "%dstdir%\css" "%srcdir%\src\stylesheets"
+
+del %dstdir%\js
+mklink /j "%dstdir%\js" "%srcdir%\src\javascript"
+
+del %dstdir%\packages
+mklink /j "%dstdir%\packages" "%srcdir%\src\packages"
+
+del %dstdir%\themes
+mklink /j "%dstdir%\themes" "%srcdir%\src\themes"
+
+npm install
+grunt
