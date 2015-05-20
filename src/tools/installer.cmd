093ed08c8470ebfeae31e9b2f64e3c1f2c8a85ea
Anders Evenrud <andersevenrud@gmail.com>
2015-05-20 22:27:56 +0200

    Updated Windows installer. Should have fixed some small errors

--- src/tools/installer.cmd
+++ src/tools/installer.cmd
@@ -5,28 +5,28 @@ REM EX: src\tools\installer.cmd
 SET srcdir=%CD%
 SET dstdir=%CD%\dist-dev
 
-del %dstdir%\blank.css
+del /N %dstdir%\blank.css
 mklink "%dstdir%\blank.css" "%srcdir%\dist\blank.css"
 
-del %dstdir%\favicon.ico
+del /N %dstdir%\favicon.ico
 mklink "%dstdir%\favicon.ico" "%srcdir%\dist\favicon.ico"
 
-del %dstdir%\favicon.png
+del /N %dstdir%\favicon.png
 mklink "%dstdir%\favicon.png" "%srcdir%\dist\favicon.png"
 
-del %dstdir%\settings.js
+del /N %dstdir%\settings.js
 mklink "%dstdir%\settings.js" "%srcdir%\dist\settings.js"
 
-rmdir %dstdir%\css
+del /N %dstdir%\css || rmdir %dstdir%\css
 mklink /j "%dstdir%\css" "%srcdir%\src\stylesheets"
 
-rmdir %dstdir%\js
+del /N %dstdir%\js || rmdir %dstdir%\js
 mklink /j "%dstdir%\js" "%srcdir%\src\javascript"
 
-rmdir %dstdir%\packages
+del /N %dstdir%\packages || rmdir %dstdir%\packages
 mklink /j "%dstdir%\packages" "%srcdir%\src\packages"
 
-rmdir %dstdir%\themes
+del /N %dstdir%\themes || rmdir %dstdir%\themes
 mklink /j "%dstdir%\themes" "%srcdir%\src\themes"
 
 npm install & grunt --force
