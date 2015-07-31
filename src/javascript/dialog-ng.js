9d9bdf89908a7016b5bf33fc923f5e9a9669df43
Anders Evenrud <andersevenrud@gmail.com>
2015-07-31 09:08:28 +0200

    gui-ng: ErrorDialog no longer shows empty traces (#136)

--- src/javascript/dialog-ng.js
+++ src/javascript/dialog-ng.js
@@ -529,31 +529,13 @@
    */
   function ErrorDialog(args, callback) {
     args = args || {};
-    DialogWindow.apply(this, ['ErrorDialog', {
-      title: args.title || API._('DIALOG_CONFIRM_TITLE'),
-      icon: 'status/dialog-error.png',
-      width: 400,
-      height: 400
-    }, args, callback]);
 
-    this._sound = 'dialog-warning';
-    this._soundVolume = 1.0;
-  }
-
-  ErrorDialog.prototype = Object.create(DialogWindow.prototype);
-  ErrorDialog.constructor = DialogWindow;
-
-  ErrorDialog.prototype.init = function() {
-    var root = DialogWindow.prototype.init.apply(this, arguments);
-
-    var exception = this.args.exception || {};
+    var exception = args.exception || {};
     var error = '';
     if ( exception.stack ) {
       error = exception.stack;
     } else {
-      if ( !Object.keys(exception).length ) {
-        error = '<unknown error>';
-      } else {
+      if ( Object.keys(exception).length ) {
         error = exception.name;
         error += '\nFilename: ' + exception.fileName || '<unknown>';
         error += '\nLine: ' + exception.lineNumber;
@@ -564,9 +546,32 @@
       }
     }
 
+    DialogWindow.apply(this, ['ErrorDialog', {
+      title: args.title || API._('DIALOG_CONFIRM_TITLE'),
+      icon: 'status/dialog-error.png',
+      width: 400,
+      height: error ? 400 : 200,
+    }, args, callback]);
+
+    this._sound = 'dialog-warning';
+    this._soundVolume = 1.0;
+
+    this.traceMessage = error;
+  }
+
+  ErrorDialog.prototype = Object.create(DialogWindow.prototype);
+  ErrorDialog.constructor = DialogWindow;
+
+  ErrorDialog.prototype.init = function() {
+    var root = DialogWindow.prototype.init.apply(this, arguments);
+
     this.scheme.find(this, 'Message').set('value', this.args.message);
     this.scheme.find(this, 'Summary').set('value', this.args.error);
-    this.scheme.find(this, 'Trace').set('value', error);
+    this.scheme.find(this, 'Trace').set('value', this.traceMessage);
+    if ( !this.traceMessage ) {
+      this.scheme.find(this, 'Trace').hide();
+      this.scheme.find(this, 'TraceLabel').hide();
+    }
 
     if ( this.args.bugreport ) {
       this.scheme.find(this, 'ButtonBugReport').on('click', function() {
