d0db8ff6728c04b0bc65f34968474861f7724e20
Anders Evenrud <andersevenrud@gmail.com>
2015-07-31 15:30:01 +0200

    gui-ng: Moved CSS stuff into Theme lesscss (#136)

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -33,7 +33,7 @@
   window.OSjs = window.OSjs || {};
 
   // Make sure these namespaces exist
-  (['API', 'Core', 'Compability', 'Helpers', 'Applications', 'Locales', 'VFS', 'Session']).forEach(function(ns) {
+  (['API', 'Core', 'Dialogs', 'Compability', 'Helpers', 'Applications', 'Locales', 'VFS', 'Session']).forEach(function(ns) {
     OSjs[ns] = OSjs[ns] || {};
   });
 
