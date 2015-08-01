0e7158efa1bc91b603e1aa1f77112262d75bd763
Anders Evenrud <andersevenrud@gmail.com>
2015-08-01 22:24:49 +0200

    gui-ng: Added About Application (#136)

---
+++ src/packages/default/About/main.js
@@ -0,0 +1,88 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+(function(Application, Window, Utils, API, VFS) {
+
+  /////////////////////////////////////////////////////////////////////////////
+  // WINDOWS
+  /////////////////////////////////////////////////////////////////////////////
+
+  function ApplicationAboutWindow(app, metadata, scheme) {
+    Window.apply(this, ['ApplicationAboutWindow', {
+      icon: metadata.icon,
+      title: metadata.name,
+      gravity: 'center',
+      allow_resize: false,
+      allow_maximize: false,
+      width: 350,
+      height: 270,
+      min_height: 270
+    }, app, scheme]);
+  }
+
+  ApplicationAboutWindow.prototype = Object.create(Window.prototype);
+  ApplicationAboutWindow.constructor = Window.prototype;
+
+  ApplicationAboutWindow.prototype.init = function(wmRef, app, scheme) {
+    var root = Window.prototype.init.apply(this, arguments);
+    scheme.render(this, 'AboutWindow', root);
+    return root;
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // APPLICATION
+  /////////////////////////////////////////////////////////////////////////////
+
+  var ApplicationAbout = function(args, metadata) {
+    Application.apply(this, ['ApplicationAbout', args, metadata]);
+  };
+
+  ApplicationAbout.prototype = Object.create(Application.prototype);
+  ApplicationAbout.constructor = Application;
+
+  ApplicationAbout.prototype.init = function(settings, metadata) {
+    Application.prototype.init.apply(this, arguments);
+
+    var self = this;
+    var url = API.getApplicationResource(this, './scheme.html');
+    var scheme = OSjs.API.createScheme(url);
+    scheme.load(function(error, result) {
+      self._addWindow(new ApplicationAboutWindow(self, metadata, scheme));
+    });
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Applications = OSjs.Applications || {};
+  OSjs.Applications.ApplicationAbout = OSjs.Applications.ApplicationAbout || {};
+  OSjs.Applications.ApplicationAbout.Class = ApplicationAbout;
+
+})(OSjs.Core.Application, OSjs.Core.Window, OSjs.Utils, OSjs.API, OSjs.VFS);
