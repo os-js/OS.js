4f654314b18d8921f5efc4f9631c661bda8e5637
Anders Evenrud <andersevenrud@gmail.com>
2015-08-02 04:36:16 +0200

    Added use strict to default application template

--- src/tools/templates/package/main.js
+++ src/tools/templates/package/main.js
@@ -28,6 +28,7 @@
  * @licence Simplified BSD License
  */
 (function(Application, Window, Utils, API, VFS) {
+  'use strict';
 
   /////////////////////////////////////////////////////////////////////////////
   // WINDOWS
