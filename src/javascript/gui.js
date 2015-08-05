675db31f467b21be5af9d86f4487c8f3f87c514d
Anders Evenrud <andersevenrud@gmail.com>
2015-08-06 01:17:39 +0200

    gui-ng: More theme updates. Added 'Dark' theme (#136)

--- src/javascript/gui.js
+++ src/javascript/gui.js
@@ -721,7 +721,7 @@
       // Apply a default className to non-containers
       node.querySelectorAll('*').forEach(function(el) {
         var lcase = el.tagName.toLowerCase();
-        if ( lcase.match(/^gui\-/) && !lcase.match(/(\-container|\-(h|v)box|\-columns?|\-rows?|bar|bar\-entry)$/) ) {
+        if ( lcase.match(/^gui\-/) && !lcase.match(/(\-container|\-(h|v)box|\-columns?|\-rows?|(status|tool)bar|(button|menu)\-bar|bar\-entry)$/) ) {
           Utils.$addClass(el, 'gui-element');
         }
       });
