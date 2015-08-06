63b211c0033414149095dbf7d6272d2d2a8e530d
Anders Evenrud <andersevenrud@gmail.com>
2015-08-06 23:35:23 +0200

    gui-ng: Now has DefaultApplication helper again

---
+++ src/javascript/helpers/default-application.js
@@ -0,0 +1,278 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+(function(Application, Window, Utils, VFS, API) {
+  'use strict';
+
+  window.OSjs       = window.OSjs       || {};
+  OSjs.Helpers      = OSjs.Helpers      || {};
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Default Application Window Helper
+  /////////////////////////////////////////////////////////////////////////////
+
+  function DefaultApplicationWindow(name, app, args, scheme, file) {
+    Window.apply(this, arguments);
+    this.currentFile = file ? new VFS.File(file) : null;
+  }
+
+  DefaultApplicationWindow.prototype = Object.create(Window.prototype);
+  DefaultApplicationWindow.constructor = Window;
+
+  DefaultApplicationWindow.prototype.init = function(wm, app, scheme) {
+    var root = Window.prototype.init.apply(this, arguments);
+    return root;
+  };
+
+  DefaultApplicationWindow.prototype._inited = function() {
+    var result = Window.prototype._inited.apply(this, arguments);
+    var self = this;
+    var app = this._app;
+
+    var menuMap = {
+      MenuNew:    function() { app.newDialog(self.currentFile, self); },
+      MenuSave:   function() { app.saveDialog(self.currentFile, self); },
+      MenuSaveAs: function() { app.saveDialog(self.currentFile, self, true); },
+      MenuOpen:   function() { app.openDialog(self.currentFile, self); },
+      MenuClose:  function() { self._close(); }
+    };
+
+    this._scheme.find(this, 'SubmenuFile').on('select', function(ev) {
+      if ( menuMap[ev.detail.id] ) { menuMap[ev.detail.id](); }
+    });
+
+    this._scheme.find(this, 'MenuSave').set('disabled', true);
+
+    // Load given file
+    if ( this.currentFile ) {
+      if ( !this._app.openFile(this.currentFile, this) ) {
+        this.currentFile = null;
+      }
+    }
+
+    return result;
+  };
+
+  DefaultApplicationWindow.prototype._onDndEvent = function(ev, type, item, args) {
+    if ( !Window.prototype._onDndEvent.apply(this, arguments) ) { return; }
+
+    if ( type === 'itemDrop' && item ) {
+      var data = item.data;
+      if ( data && data.type === 'file' && data.mime ) {
+        this._app.openFile(new VFS.File(data), this);
+      }
+    }
+  };
+
+  DefaultApplicationWindow.prototype.destroy = function() {
+    Window.prototype.destroy.apply(this, arguments);
+    this.currentFile = null;
+  };
+
+  DefaultApplicationWindow.prototype._onDndEvent = function(ev, type, item, args) {
+    if ( !Window.prototype._onDndEvent.apply(this, arguments) ) { return; }
+
+    if ( type === 'itemDrop' && item ) {
+      var data = item.data;
+      if ( data && data.type === 'file' && data.mime ) {
+        this._app.openFile(new VFS.File(data), this);
+      }
+    }
+  };
+
+  DefaultApplicationWindow.prototype.showFile = function(file, content) {
+    this.updateFile(file);
+  };
+
+  DefaultApplicationWindow.prototype.updateFile = function(file) {
+    this.currentFile = file || null;
+
+    this._scheme.find(this, 'MenuSave').set('disabled', !file);
+
+    if ( file ) {
+      this._setTitle(file.filename, true);
+    } else {
+      this._setTitle();
+    }
+  };
+
+  DefaultApplicationWindow.prototype.getFileData = function() {
+    return null;
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Default Application Helper
+  /////////////////////////////////////////////////////////////////////////////
+
+  function DefaultApplication(name, args, metadata, opts) {
+    this.defaultOptions = Utils.argumentDefaults(opts, {
+      extension: '',
+      mime: 'application/octet-stream',
+      filename: 'New file'
+    });
+
+    Application.apply(this, [name, args, metadata]);
+  }
+
+  DefaultApplication.prototype = Object.create(Application.prototype);
+  DefaultApplication.constructor = Application;
+
+  DefaultApplication.prototype._onMessage = function(obj, msg, args) {
+    Application.prototype._onMessage.apply(this, arguments);
+
+    var self = this;
+    var current = this._getArgument('file');
+    var win = this._getWindow('DefaultApplicationWindow');
+
+    if ( msg === 'vfs' && args.source !== null && args.source !== this.__pid && args.file ) {
+      if ( win && current && current.path === args.file.path ) {
+        win._toggleDisabled(true);
+        API.createDialog('Confirm', {
+          message: API._('MSG_FILE_CHANGED'),
+        }, function(ev, button) {
+          win._toggleDisabled(false);
+          if ( button === 'ok' || button === 'yes' ) {
+            self.openFile(new VFS.File(args.file), win);
+          }
+        }, win);
+      }
+    }
+  };
+
+  DefaultApplication.prototype.openFile = function(file, win) {
+    var self = this;
+    if ( !file ) { return; }
+
+    var check = this.__metadata.mime || [];
+    if ( !Utils.checkAcceptMime(file.mime, check) ) {
+      API.error(this.__label,
+                API._('ERR_FILE_APP_OPEN'),
+                API._('ERR_FILE_APP_OPEN_FMT',
+                file.path, file.mime)
+      );
+      return false;
+    }
+
+    win._toggleLoading(true);
+    VFS.read(file, function(error, result) {
+      win._toggleLoading(false);
+
+      if ( error ) {
+        API.error(this.__label,
+                  API._('ERR_FILE_APP_OPEN'),
+                  API._('ERR_FILE_APP_OPEN_ALT_FMT',
+                  file.path));
+        return;
+      }
+
+      self._setArgument('file', file);
+      win.showFile(file, result);
+    }, {type: 'text'});
+
+    return true;
+  };
+
+  DefaultApplication.prototype.saveFile = function(file, value, win) {
+    var self = this;
+    if ( !file ) { return; }
+
+    win._toggleLoading(true);
+    VFS.write(file, value || '', function(error, result) {
+      win._toggleLoading(false);
+
+      if ( error ) {
+        API.error(this.__label,
+                  API._('ERR_FILE_APP_SAVE'),
+                  API._('ERR_FILE_APP_SAVE_ALT_FMT',
+                  file.path));
+        return;
+      }
+
+      self._setArgument('file', file);
+      win.updateFile(file);
+    }, {}, this);
+  };
+
+  DefaultApplication.prototype.saveDialog = function(file, win, saveAs) {
+    var self = this;
+    var value = win.getFileData();
+
+    if ( !saveAs ) {
+      this.saveFile(file, value, win);
+      return;
+    }
+
+    win._toggleDisabled(true);
+    API.createDialog('File', {
+      file: file,
+      filename: file ? file.filename : this.defaultOptions.filename,
+      filter: this.__metadata.mime,
+      extension: this.defaultOptions.extension,
+      mime: this.defaultOptions.mime,
+      type: 'save'
+    }, function(ev, button, result) {
+      win._toggleDisabled(false);
+      if ( button === 'ok' ) {
+        self.saveFile(result, value, win);
+      }
+    }, win);
+  };
+
+  DefaultApplication.prototype.openDialog = function(file, win) {
+    var self = this;
+
+    win._toggleDisabled(true);
+    API.createDialog('File', {
+      file: file,
+      filter: this.__metadata.mime
+    }, function(ev, button, result) {
+      win._toggleDisabled(false);
+      if ( result ) {
+        self.openFile(new VFS.File(result), win);
+      }
+    }, win);
+  };
+
+  DefaultApplication.prototype.newDialog = function(path, win) {
+    var self = this;
+
+    this._setArgument('file', null);
+    win.showFile(null, null);
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Helpers.DefaultApplication       = DefaultApplication;
+  OSjs.Helpers.DefaultApplicationWindow = DefaultApplicationWindow;
+
+})(OSjs.Core.Application, OSjs.Core.Window, OSjs.Utils, OSjs.VFS, OSjs.API);
+
--- src/packages/default/Textpad/main.js
+++ src/packages/default/Textpad/main.js
@@ -27,7 +27,7 @@
  * @author  Anders Evenrud <andersevenrud@gmail.com>
  * @licence Simplified BSD License
  */
-(function(Application, Window, Utils, API, VFS, GUI) {
+(function(DefaultApplication, DefaultApplicationWindow, Application, Window, Utils, API, VFS, GUI) {
   'use strict';
 
   /////////////////////////////////////////////////////////////////////////////
@@ -35,97 +35,53 @@
   /////////////////////////////////////////////////////////////////////////////
 
   function ApplicationTextpadWindow(app, metadata, scheme, file) {
-    Window.apply(this, ['ApplicationTextpadWindow', {
+    DefaultApplicationWindow.apply(this, ['ApplicationTextpadWindow', {
       allow_drop: true,
       icon: metadata.icon,
       title: metadata.name,
       width: 450,
       height: 300
-    }, app, scheme]);
-
-    this.currentFile = file ? new VFS.File(file) : null;
+    }, app, scheme, file]);
   }
 
-  ApplicationTextpadWindow.prototype = Object.create(Window.prototype);
-  ApplicationTextpadWindow.constructor = Window.prototype;
+  ApplicationTextpadWindow.prototype = Object.create(DefaultApplicationWindow.prototype);
+  ApplicationTextpadWindow.constructor = DefaultApplicationWindow.prototype;
 
   ApplicationTextpadWindow.prototype.init = function(wmRef, app, scheme) {
-    var root = Window.prototype.init.apply(this, arguments);
+    var root = DefaultApplicationWindow.prototype.init.apply(this, arguments);
     var self = this;
 
     // Load and set up scheme (GUI) here
     scheme.render(this, 'TextpadWindow', root);
 
-    var input = scheme.find(this, 'Text');
-
-    this._scheme.find(this, 'MenuSave').set('disabled', true);
-
-    var menuMap = {
-      MenuNew:    function() { app.newDialog(self.currentFile, self); },
-      MenuSave:   function() { app.saveDialog(self.currentFile, input.get('value'), self); },
-      MenuSaveAs: function() { app.saveDialog(self.currentFile, input.get('value'), self, true); },
-      MenuOpen:   function() { app.openDialog(self.currentFile, self); },
-      MenuClose:  function() { self._close(); }
-    };
-
-    scheme.find(this, 'SubmenuFile').on('select', function(ev) {
-      if ( menuMap[ev.detail.id] ) { menuMap[ev.detail.id](); }
-    });
-
-    // Load given file
-    if ( this.currentFile ) {
-      if ( !app.openFile(this.currentFile, this) ) {
-        this.currentFile = null;
-      }
-    }
-
     return root;
   };
 
-  ApplicationTextpadWindow.prototype.update = function(file) {
-    if ( file ) {
-      this._setTitle(file.filename, true);
-    } else {
-      this._setTitle();
-    }
-    this.currentFile = file || null;
-
-    var input = this._scheme.find(this, 'Text');
-    input.$element.focus();
-
-    this._scheme.find(this, 'MenuSave').set('disabled', !file);
+  ApplicationTextpadWindow.prototype.updateFile = function(file) {
+    DefaultApplicationWindow.prototype.updateFile.apply(this, arguments);
+    this._scheme.find(this, 'Text').$element.focus();
   };
 
-  ApplicationTextpadWindow.prototype.display = function(file, content) {
-    var temp = this._scheme.find(this, 'Text').set('value', content || '');
-    this.update(file);
+  ApplicationTextpadWindow.prototype.showFile = function(file, content) {
+    this._scheme.find(this, 'Text').set('value', content || '');
+    DefaultApplicationWindow.prototype.showFile.apply(this, arguments);
+  };
+
+  ApplicationTextpadWindow.prototype.getFileData = function() {
+    return this._scheme.find(this, 'Text').get('value');
   };
 
   ApplicationTextpadWindow.prototype._focus = function() {
-    if ( Window.prototype._focus.apply(this, arguments) ) {
-      var input = this._scheme.find(this, 'Text').$element;
-      if ( input ) {
-        input.focus();
+    if ( DefaultApplicationWindow.prototype._focus.apply(this, arguments) ) {
+      if ( this._scheme ) {
+        var input = this._scheme.find(this, 'Text').$element;
+        if ( input ) {
+          input.focus();
+        }
       }
       return true;
     }
     return false;
-  };
-
-  ApplicationTextpadWindow.prototype._onDndEvent = function(ev, type, item, args) {
-    if ( !Window.prototype._onDndEvent.apply(this, arguments) ) { return; }
-
-    if ( type === 'itemDrop' && item ) {
-      var data = item.data;
-      if ( data && data.type === 'file' && data.mime ) {
-        this._app.openFile(new VFS.File(data), this);
-      }
-    }
-  };
-
-  ApplicationTextpadWindow.prototype.destroy = function() {
-    Window.prototype.destroy.apply(this, arguments);
-    this.currentFile = null;
   };
 
   /////////////////////////////////////////////////////////////////////////////
@@ -133,18 +89,18 @@
   /////////////////////////////////////////////////////////////////////////////
 
   var ApplicationTextpad = function(args, metadata) {
-    Application.apply(this, ['ApplicationTextpad', args, metadata]);
+    DefaultApplication.apply(this, ['ApplicationTextpad', args, metadata, {
+      extension: 'txt',
+      mime: 'text/plain',
+      filename: 'New text file.txt'
+    }]);
   };
 
-  ApplicationTextpad.prototype = Object.create(Application.prototype);
-  ApplicationTextpad.constructor = Application;
-
-  ApplicationTextpad.prototype.destroy = function() {
-    return Application.prototype.destroy.apply(this, arguments);
-  };
+  ApplicationTextpad.prototype = Object.create(DefaultApplication.prototype);
+  ApplicationTextpad.constructor = DefaultApplication;
 
   ApplicationTextpad.prototype.init = function(settings, metadata) {
-    Application.prototype.init.apply(this, arguments);
+    DefaultApplication.prototype.init.apply(this, arguments);
 
     var self = this;
     var url = API.getApplicationResource(this, './scheme.html');
@@ -155,127 +111,6 @@
     });
   };
 
-  ApplicationTextpad.prototype.openFile = function(file, win) {
-    var self = this;
-    if ( !file ) { return; }
-
-    var check = this.__metadata.mime || [];
-    if ( !Utils.checkAcceptMime(file.mime, check) ) {
-      API.error(this.__label,
-                API._('ERR_FILE_APP_OPEN'),
-                API._('ERR_FILE_APP_OPEN_FMT',
-                file.path, file.mime)
-      );
-      return false;
-    }
-
-    win._toggleLoading(true);
-    VFS.read(file, function(error, result) {
-      win._toggleLoading(false);
-
-      if ( error ) {
-        API.error(this.__label,
-                  API._('ERR_FILE_APP_OPEN'),
-                  API._('ERR_FILE_APP_OPEN_ALT_FMT',
-                  file.path));
-        return;
-      }
-
-      self._setArgument('file', file);
-      win.display(file, result);
-    }, {type: 'text'});
-
-    return true;
-  };
-
-  ApplicationTextpad.prototype.saveFile = function(file, value, win) {
-    var self = this;
-    if ( !file ) { return; }
-
-    win._toggleLoading(true);
-    VFS.write(file, value || '', function(error, result) {
-      win._toggleLoading(false);
-
-      if ( error ) {
-        API.error(this.__label,
-                  API._('ERR_FILE_APP_SAVE'),
-                  API._('ERR_FILE_APP_SAVE_ALT_FMT',
-                  file.path));
-        return;
-      }
-
-      self._setArgument('file', file);
-      win.update(file);
-    }, {}, this);
-  };
-
-  ApplicationTextpad.prototype.saveDialog = function(file, value, win, saveAs) {
-    var self = this;
-    if ( !saveAs ) {
-      this.saveFile(file, value, win);
-      return;
-    }
-
-    win._toggleDisabled(true);
-    API.createDialog('File', {
-      file: file,
-      filename: file ? file.filename : 'New text file.txt',
-      filter: this.__metadata.mime,
-      extension: 'txt',
-      mime: 'text/plain',
-      type: 'save'
-    }, function(ev, button, result) {
-      win._toggleDisabled(false);
-      if ( button === 'ok' ) {
-        self.saveFile(result, value, win);
-      }
-    }, win);
-  };
-
-  ApplicationTextpad.prototype.openDialog = function(file, win) {
-    var self = this;
-
-    win._toggleDisabled(true);
-    API.createDialog('File', {
-      file: file,
-      filter: this.__metadata.mime
-    }, function(ev, button, result) {
-      win._toggleDisabled(false);
-      if ( result ) {
-        self.openFile(new VFS.File(result), win);
-      }
-    }, win);
-  };
-
-  ApplicationTextpad.prototype.newDialog = function(path, win) {
-    var self = this;
-
-    this._setArgument('file', null);
-    win.display(null, null);
-  };
-
-  ApplicationTextpad.prototype._onMessage = function(obj, msg, args) {
-    Application.prototype._onMessage.apply(this, arguments);
-
-    var self = this;
-    var current = this._getArgument('file');
-    var win = this._getWindow('ApplicationTextpadWindow');
-
-    if ( msg === 'vfs' && args.source !== null && args.source !== this.__pid && args.file ) {
-      if ( win && current && current.path === args.file.path ) {
-        win._toggleDisabled(true);
-        API.createDialog('Confirm', {
-          message: API._('MSG_FILE_CHANGED'),
-        }, function(ev, button) {
-          win._toggleDisabled(false);
-          if ( button === 'ok' || button === 'yes' ) {
-            self.openFile(new VFS.File(args.file), win);
-          }
-        }, win);
-      }
-    }
-  };
-
   /////////////////////////////////////////////////////////////////////////////
   // EXPORTS
   /////////////////////////////////////////////////////////////////////////////
@@ -284,4 +119,4 @@
   OSjs.Applications.ApplicationTextpad = OSjs.Applications.ApplicationTextpad || {};
   OSjs.Applications.ApplicationTextpad.Class = ApplicationTextpad;
 
-})(OSjs.Core.Application, OSjs.Core.Window, OSjs.Utils, OSjs.API, OSjs.VFS, OSjs.GUI);
+})(OSjs.Helpers.DefaultApplication, OSjs.Helpers.DefaultApplicationWindow, OSjs.Core.Application, OSjs.Core.Window, OSjs.Utils, OSjs.API, OSjs.VFS, OSjs.GUI);
