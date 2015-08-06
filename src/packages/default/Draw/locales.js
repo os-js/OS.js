f710faaac8e82c9e93353210dda6d0e9c3fde6af
Anders Evenrud <andersevenrud@gmail.com>
2015-08-06 22:47:18 +0200

    gui-ng: Added first iteration of Draw application (#136)

---
+++ src/packages/default/Draw/locales.js
@@ -0,0 +1,337 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+(function(Application, Window, GUI, Dialogs, Utils, API, VFS) {
+  'use strict';
+
+  /////////////////////////////////////////////////////////////////////////////
+  // LOCALES
+  /////////////////////////////////////////////////////////////////////////////
+
+  var _Locales = {
+    no_NO : {
+      'Toggle tools toolbar' : 'Svitsj verktøylinje',
+      'Toggle layers toolbar' : 'Svitsj lag-verktøylinje',
+      'Layer' : 'Lag',
+      'Effect' : 'Effekt',
+      'Flip Horizontally' : 'Flipp Horisontalt',
+      'Flip Vertically' : 'Flipp Vertikalt',
+      'Foreground' : 'Forgrunn',
+      'Bakgrunn' : 'Bakgrunn',
+      'Foreground (Fill) Color' : 'Forgrunn (Fyll) Farge',
+      'Background (Stroke) Color' : 'Bakgrunn (Strøk) Farge',
+      'Line Join' : 'Linje Knytting',
+      'Line Width' : 'Linje Bredde',
+      'Toggle Stroke' : 'Svitsj strøk',
+      'Enable stroke' : 'Skru på strøk',
+      'Round' : 'Rund',
+      'Miter' : 'Skjev',
+      'Bevel' : 'Kantet',
+      'Stroked' : 'Strøk På',
+      'No stroke' : 'Strøk Av',
+
+      'Pointer' : 'Peker',
+      'Move active layer' : 'Flytt aktivt lag',
+
+      'Picker' : 'Plukker',
+      'LMB: set fg color, RMB: set gb color' : 'LMB: sett bg farge, RMB: sett fg farge',
+
+      'Pencil' : 'Penn',
+      'LMB/RMB: Draw with fg/bg color' : 'LMB/RMB: Tegn med fg/bg farge',
+      'Path' : 'Sti',
+
+      'Square/Rectangle' : 'Firkant/Rektangel',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'LMB/RMB: Tegn med fg/bg farge, SHIFT: Tegn rektangel',
+
+      'Circle/Ellipse' : 'Sirkel/Ellipse',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'LMB/RMB: Tegn med fg/bg farge, SHIFT: Tegn ellipse',
+
+      'Blur' : 'Klatte (Blur)',
+      'Noise' : 'Støy',
+      'Invert colors' : 'Inverter farger',
+      'Grayscale' : 'Gråskala',
+      'Sharpen' : 'Skarpgjør',
+      'Simple Blur' : 'Simpel Klatte (Blur)',
+
+      'Radius' : 'Radius',
+      'Iterations' : 'Itereringer'
+    },
+    pl_PL : {
+      'Toggle tools toolbar' : 'Przełącz Pasek narzędzi',
+      'Toggle layers toolbar' : 'Przełącz Pasek warstw',
+      'Layer' : 'Warstwy',
+      'Effect' : 'Efekty',
+      'Flip Horizontally' : 'Przerzuć w poziomie',
+      'Flip Vertically' : 'Przerzuć w pionie',
+      'Foreground' : 'Pierwszy plan',
+      'Bakgrunn' : 'Tło',
+      'Line Join' : 'Rodzaj lini',
+      'Line Width' : 'Grubość',
+      'Toggle Stroke' : 'Włącz/wyłącz obramowanie',
+      'Enable stroke' : 'Włącz obramowanie',
+      'Round' : 'Zwykłe',
+      'Miter' : 'Paski',
+      'Bevel' : 'Kątownik',
+      'Stroked' : 'Obranowanie',
+      'No stroke' : 'Bez obramowania',
+
+      'Pointer' : 'Wskaźnik',
+      'Move active layer' : 'Przenieś aktywne warstwy',
+
+      'Picker' : 'Wybór',
+      'LMB: set fg color, RMB: set gb color' : 'LMB: Wstaw kolor bg, RMB: ustaw kolor fg',
+
+      'Pencil' : 'Ołówek',
+      'LMB/RMB: Draw with fg/bg color' : 'Maluj w kolorze fg/bg',
+      'Path' : 'Ścieżka',
+
+      'Square/Rectangle' : 'Kwadratowe / prostokątne',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'LMB/RMB: Maluj w kolorze fb/bg, SHIFT: Narysuj prostokąt',
+
+      'Circle/Ellipse' : 'Koło / Elipsa',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'LMB/RMB: Maluj w kolorze fb/bg, SHIFT: Narysuj elipse',
+
+      'Blur' : 'Blur',
+      'Noise' : 'Szum',
+      'Invert colors' : 'Odwróc kolory',
+      'Grayscale' : 'Skala szaroścu',
+      'Sharpen' : 'Zaostrzone',
+      'Simple Blur' : 'Łatwy Blur',
+
+      'Radius' : 'Promień',
+      'Iterations' : 'Powtórzenia'
+    },
+    de_DE : {
+      'Toggle tools toolbar' : 'Tools Toolbar',
+      'Toggle layers toolbar' : 'Ebenen Toolbar',
+      'Layer' : 'Ebene',
+      'Effect' : 'Effekt',
+      'Flip Horizontally' : 'Horizontal spiegeln',
+      'Flip Vertically' : 'Vertikal spiegeln',
+      'Foreground' : 'Vordergrund',
+      'Bakgrunn' : 'Hintergrund',
+      'Foreground (Fill) Color' : 'Vordergrund (Füll-) Farbe',
+      'Background (Stroke) Color' : 'Hintergrund (Streich-) Farbe',
+      'Line Join' : 'Linienverbindung',
+      'Line Width' : 'Linienbreite',
+      'Toggle Stroke' : 'Streichen',
+      'Enable stroke' : 'Streichen aktivieren',
+      'Round' : 'Runde',
+      'Miter' : 'Live',
+      'Bevel' : 'Schräge',
+      'Stroked' : 'Gestrichen',
+      'No stroke' : 'Nicht gestrichen',
+
+      'Pointer' : 'Zeiger',
+      'Move active layer' : 'Bewege aktive Ebene',
+
+      'Picker' : 'Wähler',
+      'LMB: set fg color, RMB: set gb color' : 'LMB: wähle Vordergrundfarbe, RMB: wähle Hintergrundfarbe',
+
+      'Pencil' : 'Stift',
+      'LMB/RMB: Draw with fg/bg color' : 'LMB/RMB: Zeichnen mit fg/bg Farbe',
+      'Path' : 'Pfad',
+
+      'Square/Rectangle' : 'Quadrat/Rechteck',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'LMB/RMB: Zeichnen mit fb/bg Farbe, SHIFT: Rechteck zeichnen',
+
+      'Circle/Ellipse' : 'Kreis/Ellipse',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'LMB/RMB: Zeichnen mit fb/bg Farbe, SHIFT: Ellipse zeichnen',
+
+      'Blur' : 'Weichzeichner (Blur)',
+      'Noise' : 'Rauschen',
+      'Invert colors' : 'Farben invertieren',
+      'Grayscale' : 'Graustufen',
+      'Sharpen' : 'Schärfen',
+      'Simple Blur' : 'Einfacher Weichzeichner (Blur)',
+
+      'Radius' : 'Radius',
+      'Iterations' : 'Iterationen'
+    },
+    fr_FR : {
+    },
+    ru_RU : {
+      'Toggle tools toolbar' : 'Панель инструментов',
+      'Toggle layers toolbar' : 'Панель слоев',
+      'Layer' : 'Слой',
+      'Effect' : 'Эффекты',
+      'Flip Horizontally' : 'Отразить горизонтально',
+      'Flip Vertically' : 'Отразить вертикально',
+      'Foreground' : 'Передний план',
+      'Bakgrunn' : 'Фон',
+      'Foreground (Fill) Color' : 'Передний план (Заливка) цвет',
+      'Background (Stroke) Color' : 'Фоновый (Обводка) цвет',
+      'Line Join' : 'Замкнутая линия',
+      'Line Width' : 'Ширина линии',
+      'Toggle Stroke' : 'Вкл/выкл обводку',
+      'Enable stroke' : 'Включить обводку',
+      'Round' : 'Закругленный',
+      'Miter' : 'Прямой',
+      'Bevel' : 'Скошенный',
+      'Stroked' : 'С обводкой',
+      'No stroke' : 'Без обводки',
+
+      'Pointer' : 'Указатель',
+      'Move active layer' : 'Перемещает активный слой',
+
+      'Picker' : 'Пипетка',
+      'LMB: set fg color, RMB: set gb color' : 'ЛКМ: устананавливает первичный цвет, ПКМ: устанавливает вторичный(фоновый) цвет',
+
+      'Pencil' : 'Карандаш',
+      'LMB/RMB: Draw with fg/bg color' : 'ЛКМ/ПКМ: Рисует первичным/вторичным цветом',
+      'Path' : 'Прямая',
+
+      'Square/Rectangle' : 'Квадрат/Прямоугольник',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'ЛКМ/ПКМ: рисует первичным/вторичным цветом квадрат, SHIFT: нарисовать прямоуголник',
+
+      'Circle/Ellipse' : 'Круг/Эллипс',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'ЛКМ/ПКМ: рисует первичным/вторичным цветом круг, SHIFT: нарисовать эллипс',
+
+      'Blur' : 'Размытие (Blur)',
+      'Noise' : 'Шум',
+      'Invert colors' : 'Инвертировать цвета',
+      'Grayscale' : 'Обесцветить',
+      'Sharpen' : 'Сточить',
+      'Simple Blur' : 'Простое размытие (Blur)',
+
+      'Radius' : 'Радиус',
+      'Iterations' : 'Итерации'
+    },
+    nl_NL : {
+      'Toggle tools toolbar' : 'Toolbar gereedschappen',
+      'Toggle layers toolbar' : 'Toolbar lagen',
+      'Layer' : 'Laag',
+      'Effect' : 'Effekten',
+      'Flip Horizontally' : 'Horizontaal spiegelen',
+      'Flip Vertically' : 'Vertikaal spiegelen',
+      'Foreground' : 'Voorgrond',
+      'Bakgrunn' : 'Achtergrond',
+      'Foreground (Fill) Color' : 'Voorgrond (vul) kleur',
+      'Background (Stroke) Color' : 'Achtergrond (penseel-) kleur',
+      'Line Join' : 'Lijnverbinding',
+      'Line Width' : 'Lijnbreedte',
+      'Toggle Stroke' : 'Penseel streek',
+      'Enable stroke' : 'Penseel aktiveren',
+      'Round' : 'Rond',
+      'Miter' : 'Live',
+      'Bevel' : 'Schuin',
+      'Stroked' : 'Gestreken',
+      'No stroke' : 'Geen penseel streken',
+
+      'Pointer' : 'Aanwijzer',
+      'Move active layer' : 'Verplaats de aktieve laag',
+
+      'Picker' : 'Kiezer',
+      'LMB: set fg color, RMB: set gb color' : 'LMB: Kies voorgrond-kleur, RMB: Kies achtergrondkleur',
+
+      'Pencil' : 'Penseel',
+      'LMB/RMB: Draw with fg/bg color' : 'LMB/RMB: Teken met voor- en achtergrondkleur',
+      'Path' : 'Pfad',
+
+      'Square/Rectangle' : 'Vierkant/rechthoek',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'LMB/RMB: Teken met voor- en achtergrondkleur, SHIFT: Rechnthoek tekenen',
+
+      'Circle/Ellipse' : 'Cirkel/elipse',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'LMB/RMB: Teken met voor- en achtergrondkleur, SHIFT: Elipse tekenen',
+
+      'Blur' : 'Vager maken (Blur)',
+      'Noise' : 'Ruis',
+      'Invert colors' : 'Kleuren inverteren',
+      'Grayscale' : 'Grijstinten',
+      'Sharpen' : 'Scherper',
+      'Simple Blur' : 'Eenvoudig vager maken (Blur)',
+
+      'Radius' : 'Radius',
+      'Iterations' : 'Doorgangen'
+    },
+    vi_VN : {
+      'Toggle tools toolbar' : 'Công cụ bật tắt thanh công cụ',
+      'Toggle layers toolbar' : 'Bật tắt cửa sổ layer',
+      'Layer' : 'Lớp',
+      'Effect' : 'Hiệu ứng',
+      'Flip Horizontally' : 'Lật ngang',
+      'Flip Vertically' : 'Lật theo chiều dọc',
+      'Foreground' : 'Nền trước',
+      'Background' : 'Nền dưới',
+      'Foreground (Fill) Color' : 'Màu nền trước',
+      'Background (Stroke) Color' : 'Màu nền dưới',
+      'Line Join' : 'Ghép đoạn thằng',
+      'Line Width' : 'Độ rộng dòng',
+      'Toggle Stroke' : 'Bật tắt nét',
+      'Enable stroke' : 'Bật nét',
+      'Round' : 'Tròn',
+      'Miter' : 'Góc',
+      'Bevel' : 'Góc xiên',
+      'Stroked' : 'Vuốt',
+      'No stroke' : 'Không vuốt',
+
+      'Pointer' : 'Con trỏ',
+      'Move active layer' : 'Di chuyển layer đang chọn',
+
+      'Picker' : 'Bảng chọn',
+      'LMB: set fg color, RMB: set gb color' : 'LMB: chọn màu fg, RMB: chọn màu gb',
+
+      'Pencil' : 'Bút chì',
+      'LMB/RMB: Draw with fg/bg color' : 'LMB/RMB: vẽ với màu fg/bg',
+      'Path' : 'Đường',
+
+      'Square/Rectangle' : 'Firkant/Rektangel',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw rectangle' : 'LMB/RMB: Tegn med fg/bg farge, SHIFT: Tegn rektangel',
+
+      'Circle/Ellipse' : 'Vuông / chữ nhật',
+      'LMB/RMB: Draw with fg/bg color, SHIFT: Draw ellipse' : 'LMB/RMB: vẽ với màu fg/bg, SHIFT: Vẽ ê-líp',
+
+      'Blur' : 'Làm mờ',
+      'Noise' : 'Làm nhiễu',
+      'Invert colors' : 'Nghịch đảo màu',
+      'Grayscale' : 'Độ xám',
+      'Sharpen' : 'Làm sắc nét',
+      'Simple Blur' : 'Làm mờ đơn giản',
+
+      'Radius' : 'Bán kính',
+      'Iterations' : 'Lặp đi lặp lại'
+    }
+
+  };
+
+  function _() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(_Locales);
+    return API.__.apply(this, args);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Applications = OSjs.Applications || {};
+  OSjs.Applications.ApplicationDraw = OSjs.Applications.ApplicationDraw || {};
+  OSjs.Applications.ApplicationDraw._ = _;
+
+})(OSjs.Helpers.DefaultApplication, OSjs.Core.Window, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
---
+++ src/packages/default/Draw/main.js
@@ -0,0 +1,424 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+(function(Application, Window, Utils, API, VFS, GUI) {
+  'use strict';
+
+  var toolEvents = {
+    pointer: {
+    },
+
+    picker: (function() {
+      var imageData;
+
+      function pick(ev, args) {
+        var value = "#000000";
+        var t = (ev.shiftKey || ev.button > 0) ? 'background' : 'foreground';
+
+        if ( !imageData ) {
+          imageData = args.ctx.getImageData(0, 0, args.canvas.width, args.canvas.height).data;
+        }
+
+        var index = ((args.pos.x + args.pos.y * args.canvas.width) * 4);
+        try {
+          value = Utils.convertToHEX({
+            r:imageData[index + 0],
+            g:imageData[index + 1],
+            b:imageData[index + 2],
+            a:imageData[index + 3]
+          });
+        } catch ( e ) {
+        }
+
+        args.win.setToolProperty(t, value);
+      }
+
+      return {
+        mousedown: pick,
+        mousemove: pick,
+        mouseup: function(ev, pos, canvas, ctx, win) {
+          imageData = null;
+        }
+      };
+    })(),
+
+    bucket: {
+      mousedown: function(ev, args) {
+        var t = (ev.shiftKey || ev.button > 0) ? 'background' : 'foreground';
+        args.ctx.fillStyle = args.win.tool[t];
+        args.ctx.fillRect(0, 0, args.canvas.width, args.canvas.height);
+      }
+    },
+
+    pencil: {
+      mousedown: function(ev, args) {
+        var t = (ev.shiftKey || ev.button > 0) ? 'background' : 'foreground';
+        args.ctx.strokeStyle = args.win.tool[t];
+      },
+      mousemove: function(ev, args) {
+        args.ctx.beginPath();
+        args.ctx.moveTo(args.pos.x-1, args.pos.y);
+        args.ctx.lineTo(args.pos.x, args.pos.y);
+        args.ctx.closePath();
+        args.ctx.stroke();
+      }
+    },
+
+    path: {
+      mousemove: function(ev, args) {
+        if ( args.tmpContext ) {
+          args.tmpContext.clearRect(0, 0, args.tmpCanvas.width, args.tmpCanvas.height);
+          args.tmpContext.beginPath();
+          args.tmpContext.moveTo(args.start.x, args.start.y);
+          args.tmpContext.lineTo(args.pos.x, args.pos.y);
+          args.tmpContext.closePath();
+          args.tmpContext.stroke();
+        }
+      }
+    },
+
+    rectangle: {
+      mousedown: function(ev, args) {
+        args.tmpContext.fillStyle = (ev.button > 0) ? args.win.tool.background : args.win.tool.foreground;
+        args.tmpContext.strokeStyle = (ev.button <= 0) ? args.win.tool.background : args.win.tool.foreground;
+      },
+      mousemove: function(ev, args) {
+        var x, y, w, h;
+
+        if ( ev.shiftKey ) {
+          x = Math.min(args.pos.x, args.start.x);
+          y = Math.min(args.pos.y, args.start.y);
+          w = Math.abs(args.pos.x - args.start.x);
+          h = Math.abs(args.pos.y - args.start.y);
+        } else {
+          x = args.start.x; //Math.min(args.pos.x, args.start.x);
+          y = args.start.y; //Math.min(args.pos.y, args.start.y);
+          w = Math.abs(args.pos.x - args.start.x) * (args.pos.x < args.start.x ? -1 : 1);
+          h = Math.abs(w) * (args.pos.y < args.start.y ? -1 : 1);
+        }
+
+        args.tmpContext.clearRect(0, 0, args.tmpCanvas.width, args.tmpCanvas.height);
+        if ( w && h ) {
+          if ( args.win.tool.lineStroke ) {
+            args.tmpContext.strokeRect(x, y, w, h);
+          }
+          args.tmpContext.fillRect(x, y, w, h);
+        }
+      }
+    },
+
+    circle: {
+      mousedown: function(ev, args) {
+        args.tmpContext.fillStyle = (ev.button > 0) ? args.win.tool.background : args.win.tool.foreground;
+        args.tmpContext.strokeStyle = (ev.button <= 0) ? args.win.tool.background : args.win.tool.foreground;
+      },
+      mousemove: function(ev, args) {
+        if ( ev.shiftKey ) {
+          var width = Math.abs(args.start.x - args.pos.x);
+          var height = Math.abs(args.start.y - args.pos.y);
+
+          args.tmpContext.clearRect(0, 0, args.tmpCanvas.width, args.tmpCanvas.height);
+          if ( width > 0 && height > 0 ) {
+            args.tmpContext.beginPath();
+            args.tmpContext.moveTo(args.start.x, args.start.y - height*2); // A1
+            args.tmpContext.bezierCurveTo(
+              args.start.x + width*2, args.start.y - height*2, // C1
+              args.start.x + width*2, args.start.y + height*2, // C2
+              args.start.x, args.start.y + height*2); // A2
+
+            args.tmpContext.bezierCurveTo(
+              args.start.x - width*2, args.start.y + height*2, // C3
+              args.start.x - width*2, args.start.y - height*2, // C4
+              args.start.x, args.start.y - height*2); // A1
+
+            args.tmpContext.closePath();
+            if ( args.win.tool.lineStroke ) {
+              args.tmpContext.stroke();
+            }
+            args.tmpContext.fill();
+          }
+        } else {
+          var x = Math.abs(args.start.x - args.pos.x);
+          var y = Math.abs(args.start.y - args.pos.y);
+          var r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+          args.tmpContext.clearRect(0, 0, args.tmpCanvas.width, args.tmpCanvas.height);
+          if ( r > 0 ) {
+            args.tmpContext.beginPath();
+            args.tmpContext.arc(args.start.x, args.start.y, r, 0, Math.PI*2, true);
+            args.tmpContext.closePath();
+
+            if ( args.win.tool.lineStroke ) {
+              args.tmpContext.stroke();
+            }
+            args.tmpContext.fill();
+          }
+        }
+      }
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // WINDOWS
+  /////////////////////////////////////////////////////////////////////////////
+
+  function ApplicationDrawWindow(app, metadata, scheme) {
+    Window.apply(this, ['ApplicationDrawWindow', {
+      icon: metadata.icon,
+      title: metadata.name,
+      allow_drop: true,
+      min_width: 400,
+      min_height: 450,
+      width: 800,
+      height: 450
+    }, app, scheme]);
+
+    this.tool = {
+      name: 'pointer',
+      background: '#ffffff',
+      foreground: '#000000',
+      lineJoin: 'round',
+      lineWidth: 1,
+      lineStroke: false
+    };
+  }
+
+  ApplicationDrawWindow.prototype = Object.create(Window.prototype);
+  ApplicationDrawWindow.constructor = Window.prototype;
+
+  ApplicationDrawWindow.prototype.init = function(wm, app, scheme) {
+    var root = Window.prototype.init.apply(this, arguments);
+    var self = this;
+
+    // Load and set up scheme (GUI) here
+    scheme.render(this, 'DrawWindow', root);
+
+    //
+    // Menu
+    //
+
+    //
+    // Canvas
+    //
+    var canvas = scheme.find(this, 'Canvas').querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var startPos = {x: 0, y: 0};
+    var cpos = {x: 0, y: 0};
+    var tmpTools = ['path', 'rectangle', 'circle'];
+    var tmpCanvas, tmpContext;
+    var startPos;
+
+    function createTempCanvas(ev) {
+      tmpCanvas = document.createElement('canvas');
+      tmpCanvas.width = canvas.width;
+      tmpCanvas.height = canvas.height;
+      tmpCanvas.style.position = 'absolute';
+      tmpCanvas.style.top = '0px';
+      tmpCanvas.style.left = '0px';
+      tmpCanvas.style.zIndex = 9999999999;
+      canvas.parentNode.appendChild(tmpCanvas);
+
+      var t = (ev.shiftKey || ev.button > 0);
+
+      tmpContext = tmpCanvas.getContext('2d');
+      tmpContext.strokeStyle = t ? ctx.fillStyle : ctx.strokeStyle;
+      tmpContext.fillStyle = t ? ctx.strokeSyle : ctx.fillStyle;
+      tmpContext.lineWidth = ctx.lineWidth;
+      tmpContext.lineJoin = ctx.lineJoin;
+    }
+
+    function removeTempCanvas() {
+      Utils.$remove(tmpCanvas);
+      tmpContext = null;
+      tmpCanvas = null;
+    }
+
+    function toolAction(action, ev, pos, diff) {
+      if ( action === 'down' ) {
+        startPos = {x: pos.x, y: pos.y};
+
+        removeTempCanvas();
+
+        var elpos = Utils.$position(canvas);
+        startPos.x = pos.x - elpos.left;
+        startPos.y = pos.y - elpos.top;
+        cpos = {x: startPos.x, y: startPos.y};
+
+        ctx.strokeStyle = self.tool.foreground;
+        ctx.fillStyle   = self.tool.background;
+        ctx.lineWidth   = self.tool.lineWidth;
+        ctx.lineJoin    = self.tool.lineJoin;
+
+        if ( tmpTools.indexOf(self.tool.name) >= 0 ) {
+          createTempCanvas(ev);
+        }
+      } else if ( action === 'move' ) {
+        cpos.x = startPos.x + diff.x;
+        cpos.y = startPos.y + diff.y;
+      } else if ( action === 'up' ) {
+        if ( tmpCanvas && ctx ) {
+          ctx.drawImage(tmpCanvas, 0, 0);
+        }
+        removeTempCanvas();
+        startPos = null;
+      }
+
+      if ( toolEvents[self.tool.name] && toolEvents[self.tool.name]['mouse' + action] ) {
+        toolEvents[self.tool.name]['mouse' + action](ev, {
+          pos: cpos,
+          start: startPos,
+          canvas: canvas,
+          ctx: ctx,
+          tmpContext: tmpContext,
+          tmpCanvas: tmpCanvas,
+          win: self
+        });
+      }
+    }
+
+    GUI.Helpers.createDrag(canvas, function(ev, pos) {
+      toolAction('down', ev, pos);
+    }, function(ev, diff, pos) {
+      toolAction('move', ev, pos, diff);
+    }, function(ev, pos) {
+      toolAction('up', ev, pos);
+    });
+
+    //
+    // Toolbars
+    //
+    scheme.find(this, 'Foreground').on('click', function() {
+      self.openColorDialog('foreground');
+    });
+    scheme.find(this, 'Background').on('click', function() {
+      self.openColorDialog('background');
+    });
+
+    var tools = ['pointer', 'picker', 'bucket', 'pencil', 'path', 'rectangle', 'circle'];
+    tools.forEach(function(t) {
+      scheme.find(self, 'tool-' + t).on('click', function() {
+        self.setToolProperty('name', t);
+      });
+    });
+
+    var lineWidths = [];
+    for ( var i = 1; i < 22; i++ ) {
+      lineWidths.push({label: i.toString(), value: i});
+    }
+
+    scheme.find(this, 'LineWidth').add(lineWidths).on('change', function(ev) {
+      self.setToolProperty('lineWidth', parseInt(ev.detail, 10));
+    });
+    scheme.find(this, 'LineJoin').on('change', function(ev) {
+      self.setToolProperty('lineJoin', ev.detail);
+    });
+    scheme.find(this, 'LineStroke').on('change', function(ev) {
+      self.setToolProperty('lineStroke', ev.detail);
+    });
+
+    //
+    // Init
+    //
+    this.setToolProperty('background', null);
+    this.setToolProperty('foreground', null);
+    this.setToolProperty('lineJoin', null);
+    this.setToolProperty('lineWidth', null);
+    this.setToolProperty('lineStroke', null);
+
+    return root;
+  };
+
+  ApplicationDrawWindow.prototype.destroy = function() {
+    Window.prototype.destroy.apply(this, arguments);
+  };
+
+  ApplicationDrawWindow.prototype.openColorDialog = function(param) {
+    var self = this;
+
+    API.createDialog('Color', {
+      title: 'Set ' + param + ' color', // FIXME: Locale
+      color: self.tool[param]
+    }, function(ev, button, result) {
+      if ( button !== 'ok' ) { return; }
+      self.setToolProperty(param, result.hex);
+    });
+  };
+
+  ApplicationDrawWindow.prototype.setToolProperty = function(param, value) {
+    console.warn('setToolProperty', param, value);
+
+    if ( typeof this.tool[param] !== 'undefined' ) {
+      if ( value !== null ) {
+        this.tool[param] = value;
+      }
+    }
+
+    this._scheme.find(this, 'Foreground').set('value', this.tool.foreground);
+    this._scheme.find(this, 'Background').set('value', this.tool.background);
+    this._scheme.find(this, 'LineJoin').set('value', this.tool.lineJoin);
+    this._scheme.find(this, 'LineWidth').set('value', this.tool.lineWidth);
+    this._scheme.find(this, 'LineStroke').set('value', this.tool.lineStroke);
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // APPLICATION
+  /////////////////////////////////////////////////////////////////////////////
+
+  var ApplicationDraw = function(args, metadata) {
+    Application.apply(this, ['ApplicationDraw', args, metadata]);
+  };
+
+  ApplicationDraw.prototype = Object.create(Application.prototype);
+  ApplicationDraw.constructor = Application;
+
+  ApplicationDraw.prototype.destroy = function() {
+    return Application.prototype.destroy.apply(this, arguments);
+  };
+
+  ApplicationDraw.prototype.init = function(settings, metadata) {
+    Application.prototype.init.apply(this, arguments);
+
+    var self = this;
+    var url = API.getApplicationResource(this, './scheme.html');
+    var scheme = GUI.createScheme(url);
+    scheme.load(function(error, result) {
+      self._addWindow(new ApplicationDrawWindow(self, metadata, scheme));
+    });
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Applications = OSjs.Applications || {};
+  OSjs.Applications.ApplicationDraw = OSjs.Applications.ApplicationDraw || {};
+  OSjs.Applications.ApplicationDraw.Class = ApplicationDraw;
+
+})(OSjs.Core.Application, OSjs.Core.Window, OSjs.Utils, OSjs.API, OSjs.VFS, OSjs.GUI);
