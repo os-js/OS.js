4edae45bf7e21d9d5359a9779001218fb6e59b40
Anders Evenrud <andersevenrud@gmail.com>
2015-08-15 16:22:07 +0200

    Merging of some GUI fuction (#136)

--- src/javascript/gui.js
+++ src/javascript/gui.js
@@ -308,36 +308,52 @@
   /**
    * Create a new custom DOM element
    *
-   * @param   String      tagName     Tag Name
-   * @param   Object      params      Dict with data-* properties
+   * @param   String      tagName           Tag Name
+   * @param   Object      params            Dict with data-* properties
+   * @param   Array       ignoreParams      (optional) list of arguments to ignore
    *
    * @return  DOMElement
    */
-  function createElement(tagName, params) {
+  function createElement(tagName, params, ignoreParams) {
+    ignoreParams = ignoreParams || [];
+
     var el = document.createElement(tagName);
 
     var classMap = {
       textalign: function(v) {
         Utils.$addClass(el, 'gui-align-' + v);
+      },
+      className: function(v) {
+        Utils.$addClass(el, v);
       }
     };
 
+    function getValue(k, value) {
+      if ( typeof value === 'boolean' ) {
+        value = value ? 'true' : 'false';
+      } else if ( typeof value === 'object' ) {
+        try {
+          value = JSON.stringify(value);
+        } catch ( e ) {}
+      }
+
+      return value;
+    }
+
     if ( typeof params === 'object' ) {
       Object.keys(params).forEach(function(k) {
+        if ( ignoreParams.indexOf(k) >= 0 ) {
+          return;
+        }
+
         var value = params[k];
         if ( classMap[k] ) {
           classMap[k](value);
           return;
         }
 
-        if ( typeof value === 'boolean' ) {
-          value = value ? 'true' : 'false';
-        } else if ( typeof value === 'object' ) {
-          try {
-            value = JSON.stringify(value);
-          } catch ( e ) {}
-        }
-        el.setAttribute('data-' + k, value);
+        var fvalue = getValue(k, value);
+        el.setAttribute('data-' + k, fvalue);
       });
     }
 
