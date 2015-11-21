5f46427620ebdb9f247e290d85f230de533deef9
Anders Evenrud <andersevenrud@gmail.com>
2015-11-21 21:22:10 +0100

    utils: Split utils.js into individual files

---
+++ src/client/javascript/utils/compability.js
@@ -0,0 +1,346 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+(function() {
+  'use strict';
+
+  window.OSjs = window.OSjs || {};
+  OSjs.Utils  = OSjs.Utils  || {};
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Gets browser compability flags
+   *
+   * @return    Object      List of compability
+   *
+   * @api       OSjs.Utils.getCompability()
+   */
+  OSjs.Utils.getCompability = (function() {
+    function _checkSupport(enabled, check, isSupported) {
+      var supported = {};
+
+      Object.keys(check).forEach(function(key) {
+        var chk = check[key];
+        var value = false;
+
+        if ( chk instanceof Array ) {
+          chk.forEach(function(c) {
+            value = isSupported(c);
+            return !value;
+          });
+        } else {
+          value = isSupported(chk);
+        }
+        supported[key] = value;
+      });
+
+      return supported;
+    }
+
+    function getUpload() {
+      try {
+        var xhr = new XMLHttpRequest();
+        return (!! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload)));
+      } catch ( e ) {}
+      return false;
+    }
+
+    function getCanvasSupported() {
+      return document.createElement('canvas').getContext ? document.createElement('canvas') : null;
+    }
+
+    function getVideoSupported() {
+      return document.createElement('video').canPlayType ? document.createElement('video') : null;
+    }
+
+    function canPlayCodec(support, check) {
+      return _checkSupport(support, check, function(codec) {
+        try {
+          return !!support.canPlayType(codec);
+        } catch ( e ) {
+        }
+        return false;
+      });
+    }
+
+    function getVideoTypesSupported() {
+      return canPlayCodec(getVideoSupported(), {
+        webm     : 'video/webm; codecs="vp8.0, vorbis"',
+        ogg      : 'video/ogg; codecs="theora"',
+        h264     : [
+          'video/mp4; codecs="avc1.42E01E"',
+          'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+        ],
+        mpeg     : 'video/mp4; codecs="mp4v.20.8"',
+        mkv      : 'video/x-matroska; codecs="theora, vorbis"'
+      });
+    }
+
+    function getAudioSupported() {
+      return document.createElement('audio').canPlayType ? document.createElement('audio') : null;
+    }
+
+    function getAudioTypesSupported() {
+      return canPlayCodec(getAudioSupported(), {
+        ogg   : 'audio/ogg; codecs="vorbis',
+        mp3   : 'audio/mpeg',
+        wav   : 'audio/wav; codecs="1"'
+      });
+    }
+
+    function getAudioContext() {
+      if ( window.hasOwnProperty('AudioContext') || window.hasOwnProperty('webkitAudioContext') ) {
+        return true;
+      }
+      return false;
+    }
+
+    var getCanvasContexts = (function() {
+      var cache = [];
+
+      return function() {
+        if ( !cache.length ) {
+          var canvas = getCanvasSupported();
+          if ( canvas ) {
+            var test = ['2d', 'webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl'];
+            test.forEach(function(tst, i) {
+              try {
+                if ( !!canvas.getContext(tst) ) {
+                  cache.push(tst);
+                }
+              } catch ( eee ) {}
+            });
+          }
+        }
+
+        return cache;
+      };
+    })();
+
+    function getWebGL() {
+      var result = false;
+      var contexts = getCanvasContexts();
+      try {
+        result = (contexts.length > 1);
+        if ( !result ) {
+          if ( 'WebGLRenderingContext' in window ) {
+            result = true;
+          }
+        }
+      } catch ( e ) {}
+      return result;
+    }
+
+    function detectCSSFeature(featurename) {
+      var feature             = false,
+          domPrefixes         = 'Webkit Moz ms O'.split(' '),
+          elm                 = document.createElement('div'),
+          featurenameCapital  = null;
+
+      featurename = featurename.toLowerCase();
+
+      if ( elm.style[featurename] ) { feature = true; }
+
+      if ( feature === false ) {
+        featurenameCapital = featurename.charAt(0).toUpperCase() + featurename.substr(1);
+        for( var i = 0; i < domPrefixes.length; i++ ) {
+          if( elm.style[domPrefixes[i] + featurenameCapital ] !== undefined ) {
+            feature = true;
+            break;
+          }
+        }
+      }
+      return feature;
+    }
+
+    function getUserMedia() {
+      var getMedia = false;
+      if ( window.navigator ) {
+        getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+      }
+      return !!getMedia;
+    }
+
+    function getRichText() {
+      try {
+        return !!document.createElement('textarea').contentEditable;
+      } catch ( e ) {}
+      return false;
+    }
+
+    function getTouch() {
+      // False positives in win 8+
+      try {
+        if ( navigator.userAgent.match(/Windows NT 6\.(2|3)/) ) {
+          return false;
+        }
+      } catch ( e ) {}
+
+      // We only want touch for mobile devices
+      try {
+        if ( navigator.userAgent.match(/iOS|Android|BlackBerry|IEMobile|iPad|iPhone|iPad/i) ) {
+          return true;
+        }
+      } catch ( e ) {}
+
+      return false;
+      // This was the old method
+      //return ('ontouchstart' in window) || (window.DocumentTouch && (document instanceof window.DocumentTouch));
+      //return (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+    }
+
+    function getDnD() {
+      return !!('draggable' in document.createElement('span'));
+    }
+
+    function getSVG() {
+      return (!!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect);
+    }
+
+    function getFileSystem() {
+      return (('requestFileSystem' in window) || ('webkitRequestFileSystem' in window));
+    }
+
+    var checkWindow = {
+      indexedDB      : 'indexedDB',
+      localStorage   : 'localStorage',
+      sessionStorage : 'sessionStorage',
+      globalStorage  : 'globalStorage',
+      openDatabase   : 'openDatabase',
+      socket         : 'WebSocket',
+      worker         : 'Worker',
+      file           : 'File',
+      blob           : 'Blob',
+      orientation    : 'onorientationchange',
+    };
+
+    var compability = {
+      touch          : getTouch(),
+      upload         : getUpload(),
+      getUserMedia   : getUserMedia(),
+      fileSystem     : getFileSystem(),
+      localStorage   : false,
+      sessionStorage : false,
+      globalStorage  : false,
+      openDatabase   : false,
+      socket         : false,
+      worker         : false,
+      file           : false,
+      blob           : false,
+      orientation    : false,
+      dnd            : getDnD(),
+      css            : {
+        transition : detectCSSFeature('transition'),
+        animation : detectCSSFeature('animation')
+      },
+      canvas         : !!getCanvasSupported(),
+      canvasContext  : getCanvasContexts(),
+      webgl          : getWebGL(),
+      audioContext   : getAudioContext(),
+      svg            : getSVG(),
+      video          : !!getVideoSupported(),
+      videoTypes     : getVideoTypesSupported(),
+      audio          : !!getAudioSupported(),
+      audioTypes     : getAudioTypesSupported(),
+      richtext       : getRichText()
+    };
+
+    Object.keys(checkWindow).forEach(function(key) {
+      compability[key] = (checkWindow[key] in window) && window[checkWindow[key]] !== null;
+    });
+
+    return function() {
+      return compability;
+    };
+  })();
+
+  /**
+   * Check if browser is IE
+   *
+   * @return    boolean       If IE
+   *
+   * @api       OSjs.Utils.isIE()
+   */
+  OSjs.Utils.isIE = function() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') !== -1) ? parseInt(myNav.split('msie')[1], 10) : false;
+  };
+
+  /**
+   * Gets the browser Locale
+   *
+   * For example 'en_EN'
+   *
+   * @return  String          Locale string
+   *
+   * @api     OSjs.Utils.getUserLocale()
+   */
+  OSjs.Utils.getUserLocale = function() {
+    var loc = ((window.navigator.userLanguage || window.navigator.language) || 'en-EN').replace('-', '_');
+
+    // Restricts to a certain type of language.
+    // Example: There are lots of variants of the English language, but currently we only
+    // provide locales for one of them, so we force to use the one available.
+    var map = {
+      'nb'    : 'no_NO',
+      'es'    : 'es_ES',
+      'ru'    : 'ru_RU',
+      'en'    : 'en_EN'
+    };
+
+    var major = loc.split('_')[0] || 'en';
+    var minor = loc.split('_')[1] || major.toUpperCase();
+    if ( map[major] ) {
+      return map[major];
+    }
+    return major + '_' + minor;
+  };
+
+  /**
+   * Gets the browser window rect (x, y, width, height)
+   *
+   * @api OSjs.Utils.getRect()
+   * @return Object
+   */
+  OSjs.Utils.getRect = function() {
+    return {
+      top    : 0,
+      left   : 0,
+      width  : window.innerWidth,
+      height : window.innerHeight
+    };
+  };
+
+})();
---
+++ src/client/javascript/utils/misc.js
@@ -0,0 +1,260 @@
+/*!
+ * OS.js - JavaScript Operating System
+ *
+ * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+(function() {
+  'use strict';
+
+  window.OSjs = window.OSjs || {};
+  OSjs.Utils  = OSjs.Utils  || {};
+
+  /////////////////////////////////////////////////////////////////////////////
+  // STRING
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Format a string (almost like sprintf)
+   *
+   * @param   String      format        String format
+   * @param   String      ...           Insert into format
+   *
+   * @return  String                    The formatted string
+   *
+   * @link    http://stackoverflow.com/a/4673436
+   * @api     OSjs.Utils.format()
+   */
+  OSjs.Utils.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    var sprintfRegex = /\{(\d+)\}/g;
+
+    function sprintf(match, number) {
+      return number in args ? args[number] : match;
+    }
+
+    return format.replace(sprintfRegex, sprintf);
+  };
+
+  /**
+   * Remove whitespaces and newlines from HTML document
+   *
+   * @param   String    html          HTML string input
+   *
+   * @return  String
+   *
+   * @api     OSjs.Utils.cleanHTML()
+   */
+  OSjs.Utils.cleanHTML = function(html) {
+    return html.replace(/\n/g, '')
+               .replace(/[\t ]+</g, '<')
+               .replace(/\>[\t ]+</g, '><')
+               .replace(/\>[\t ]+$/g, '>');
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // OBJECT HELPERS
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Wrapper for merging function argument dictionaries
+   *
+   * @api    OSjs.Utils.argumentDefaults()
+   *
+   * @param  Object   args      Given function Dictionary
+   * @param  Object   defaults  Defaults Dictionary
+   * @param  boolean  undef     Check with 'undefined'
+   * @return Object
+   */
+  OSjs.Utils.argumentDefaults = function(args, defaults, undef) {
+    args = args || {};
+    Object.keys(defaults).forEach(function(key) {
+      if ( typeof defaults[key] === 'boolean' || typeof defaults[key] === 'number' ) {
+        if ( typeof args[key] === 'undefined' || args[key] === null ) {
+          args[key] = defaults[key];
+        }
+      } else {
+        args[key] = args[key] || defaults[key];
+      }
+    });
+    return args;
+  };
+
+  /**
+   * Deep-merge to objects
+   *
+   * @param   Object      obj1      Object to merge to
+   * @param   Object      obj2      Object to merge with
+   *
+   * @return  Object                The merged object
+   *
+   * @api     OSjs.Utils.mergeObject()
+   */
+  OSjs.Utils.mergeObject = function(obj1, obj2, opts) {
+    opts = opts || {};
+
+    for ( var p in obj2 ) {
+      if ( obj2.hasOwnProperty(p) ) {
+        try {
+          if (opts.overwrite === false && obj1.hasOwnProperty(p)) {
+            continue;
+          }
+
+          if ( obj2[p].constructor === Object ) {
+            obj1[p] = OSjs.Utils.mergeObject(obj1[p], obj2[p]);
+          } else {
+            obj1[p] = obj2[p];
+          }
+        } catch(e) {
+          obj1[p] = obj2[p];
+        }
+      }
+    }
+    return obj1;
+  };
+
+  /**
+   * Clone a object
+   *
+   * @param   Object      o     The object to clone
+   *
+   * @return  Object            An identical object
+   *
+   * @api     OSjs.Utils.cloneObject()
+   */
+  OSjs.Utils.cloneObject = function(o) {
+    return JSON.parse(JSON.stringify(o, function(key, value) {
+      if ( value && typeof value === 'object' && value.tagName ) {
+        return undefined;
+      }
+      return value;
+    }));
+  };
+
+  /**
+   * Fixes JSON for HTTP requests (in case they were returned as string)
+   *
+   * @param   Mixed     response      The data
+   *
+   * @return  Object                  JSON
+   *
+   * @api     OSjs.Utils.fixJSON()
+   */
+  OSjs.Utils.fixJSON = function(response) {
+    if ( typeof response === 'string' ) {
+      if ( response.match(/^\{|\[/) ) {
+        try {
+          response = JSON.parse(response);
+        } catch ( e  ){
+          console.warn('FAILED TO FORCE JSON MIME TYPE', e);
+        }
+      }
+    }
+    return response;
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // COLORS
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Convert HEX to RGB
+   *
+   * @param   String      hex     The hex string (with #)
+   *
+   * @return  Object              RGB in form of {r:0, g:0, b:0}
+   *
+   * @api     OSjs.Utils.convertToRGB()
+   */
+  OSjs.Utils.convertToRGB = function(hex) {
+    var rgb = parseInt(hex.replace('#', ''), 16);
+    var val = {};
+    val.r = (rgb & (255 << 16)) >> 16;
+    val.g = (rgb & (255 << 8)) >> 8;
+    val.b = (rgb & 255);
+    return val;
+  };
+
+  /**
+   * Convert RGB to HEX
+   *
+   * @param   Object      rgb       The RGB object in form of {r:0, g:0, b:0}
+   *
+   * OR
+   *
+   * @param   int         r         Red value
+   * @param   int         g         Green value
+   * @param   int         b         Blue value
+   *
+   * @return  String                Hex string (with #)
+   *
+   * @api     OSjs.Utils.convertToHEX()
+   */
+  OSjs.Utils.convertToHEX = function(r, g, b) {
+    if ( typeof r === 'object' ) {
+      g = r.g;
+      b = r.b;
+      r = r.r;
+    }
+
+    if ( typeof r === 'undefined' || typeof g === 'undefined' || typeof b === 'undefined' ) {
+      throw new Error('Invalid RGB supplied to convertToHEX()');
+    }
+
+    var hex = [
+      parseInt(r, 10).toString( 16 ),
+      parseInt(g, 10).toString( 16 ),
+      parseInt(b, 10).toString( 16 )
+    ];
+
+    Object.keys(hex).forEach(function(i) {
+      if ( hex[i].length === 1 ) {
+        hex[i] = '0' + hex[i];
+      }
+    });
+
+    return '#' + hex.join('').toUpperCase();
+  };
+
+  /**
+   * Ivert HEX color
+   *
+   * @param   String      hex     Hex string (With #)
+   *
+   * @return  String              Inverted hex (With #)
+   *
+   * @link    http://stackoverflow.com/a/9601429/1236086
+   * @api     OSjs.Utils.invertHEX()
+   */
+  OSjs.Utils.invertHEX = function(hex) {
+    var color = parseInt(hex.replace('#', ''), 16);
+    color = 0xFFFFFF ^ color;
+    color = color.toString(16);
+    color = ('000000' + color).slice(-6);
+    return '#' + color;
+  };
+
+})();
