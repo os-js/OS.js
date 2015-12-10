1df0018a356eb99b9adcbe6268a12e26dca3e921
Anders Evenrud <andersevenrud@gmail.com>
2015-12-10 18:27:00 +0100

    Now using os.js.org repository build

--- gendoc/update.sh
+++
@@ -1,5 +0,0 @@
-node generate.js
-rm output/client/*
-node generate-client.js
-node generate-sitemap.js
-mv output/sitemap.xml ../sitemap.xml
---
+++ windows-installer.cmd
@@ -0,0 +1,3 @@
+REM Helper for launching automated installer
+
+PowerShell.exe -ExecutionPolicy ByPass -File windows-installer.ps1
