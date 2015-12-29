63723066e40b07622ef90c9a577a59110a1a33cc
Anders Evenrud <andersevenrud@gmail.com>
2015-12-29 22:55:59 +0100

    You can now run server helper scripts from any cwd

--- bin/start-node-dev.sh
+++ bin/start-node-dev.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Make sure it is executed from root path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $(dirname $DIR)
+
 if ! type "supervisor" > /dev/null 2>&1; then
   echo "Starting Node server without a supervisor..."
   node src/server/node/server.js dist-dev
--- bin/start-node-dist.sh
+++ bin/start-node-dist.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# Make sure it is executed from root path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $(dirname $DIR)
+
 node src/server/node/server.js
