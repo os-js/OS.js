b4d4c885329a3eb613777a4bed4432388ae50598
Anders Evenrud <andersevenrud@gmail.com>
2015-12-30 22:29:21 +0100

    Second try to fix cwd in windows batch scripts

--- bin/win-start-node-dev.cmd
+++ bin/win-start-node-dev.cmd
@@ -3,4 +3,4 @@ for %%B in (%~dp0\.) do set c=%%~dpB
 ECHO This script will launch OS.js in node using: `node src\server\node\server.js dist-dev`
 ECHO To stop node server, press CTRL+C
 pause
-node "%c%\src\server\node\server.js dist-dev"
+node "%c%\src\server\node\server.js" dist-dev
