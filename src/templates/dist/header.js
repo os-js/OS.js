1736cb069ddcc797350557d2842fa55787e80055
Anders Evenrud <andersevenrud@gmail.com>
2016-01-01 03:01:49 +0100

    Updated copyright headers

--- src/templates/dist/header.js
+++ src/templates/dist/header.js
@@ -5,7 +5,7 @@ var OSjs;
 /**
  * @preserve OS.js - JavaScript Operating System
  *
- * Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
+ * Copyright (c) 2011-2016, Anders Evenrud <andersevenrud@gmail.com>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
