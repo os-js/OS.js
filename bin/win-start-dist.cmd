9fdf26a10a0612c6ea980cb19eb2106a2607bb82
alex <alexandre.mangin@viacesi.fr>
2016-01-14 00:07:21 +0100

    Update windows batch script for handle supervisor for the live reload

--- bin/win-start-dist.cmd
+++ bin/win-start-dist.cmd
@@ -1,6 +1,13 @@
 @ECHO OFF
 for %%i in ("%~dp0..") do set "folder=%%~fi"
-ECHO This script will launch OS.js in node using: `node src\server\node\server.js dist`
+ECHO This script will launch OS.js in node using: `node src\server\node\server.js dist-dev`
 ECHO To stop node server, press CTRL+C
-pause
-node "%folder%\src\server\node\server.js"
+
+where /q supervisor
+IF ERRORLEVEL 1 (
+    ECHO supervisor not installed, the server will not be live reloaded
+    node "%folder%\src\server\node\server.js" dist-dev
+) ELSE (
+    ECHO supervisor is installed, live reload is active
+    supervisor --watch "%folder%\src\server\node","%folder%\src\server\settings.json" -- "%folder%\src\server\node\server.js" dist-dev
+)
