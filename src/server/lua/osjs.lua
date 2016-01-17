0fb28ffdbbc3609ce23edea4c6041b7e6bd59af9
Arturo Rinaldi <arty.net2@gmail.com>
2016-01-17 02:27:02 +0100

    setting hostname to wide-system configuration

--- src/server/lua/osjs.lua
+++ src/server/lua/osjs.lua
@@ -3,16 +3,16 @@
 --
 -- Copyright (c) 2011-2015, Anders Evenrud <andersevenrud@gmail.com>
 -- All rights reserved.
--- 
+--
 -- Redistribution and use in source and binary forms, with or without
--- modification, are permitted provided that the following conditions are met: 
--- 
+-- modification, are permitted provided that the following conditions are met:
+--
 -- 1. Redistributions of source code must retain the above copyright notice, this
---    list of conditions and the following disclaimer. 
+--    list of conditions and the following disclaimer.
 -- 2. Redistributions in binary form must reproduce the above copyright notice,
 --    this list of conditions and the following disclaimer in the documentation
---    and/or other materials provided with the distribution. 
--- 
+--    and/or other materials provided with the distribution.
+--
 -- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 -- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 -- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -638,6 +638,8 @@ function api_request(request, response, meth, iargs)
       fs.writefile("/etc/TZ", timezone)
     end
 
+    console("/sbin/uci set system.@system[0].hostname=" .. hostname)
+    console("/sbin/uci commit system")
     data = true
   elseif meth == "reboot" then
     sys.reboot()
@@ -725,4 +727,3 @@ return {
   get_user_home = get_user_home,
   get_user_settings_path = get_user_settings_path
 }
-
