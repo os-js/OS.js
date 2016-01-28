ab81f48ea87b0a2620f67db503055065e19ea84c
Anders Evenrud <andersevenrud@gmail.com>
2016-01-28 20:34:16 +0100

    lua-server: Fixed .. in scandir (2)

--- src/server/lua/osjs.lua
+++ src/server/lua/osjs.lua
@@ -224,28 +224,19 @@ function fs_scandir(request, response, path)
   local error = false
   local data = {}
 
-  local function splitpath(P)
-    local i = #P
-    local ch = string.sub(P,i,i)
-    while i > 0 and ch ~= "/" do
-        i = i - 1
-        ch = string.sub(P,i,i)
-    end
-    if i == 0 then
-        return '',P
-    else
-        return string.sub(P,1,i-1), string.sub(P,i+1)
-    end
-  end
-
   local realpath = get_real_path(request, response, path)
   local tmppath = string.gsub(path, "^((%a+)://)", "")
   tmppath = string.gsub(tmppath, "^\/+", "")
 
   if tmppath ~= "" then
-    local p1,p2 = splitpath(P)
+    local pmatch = string.match(path, "^((%a+)://)")
+    local protocol = string.gsub(pmatch, "://", "")
+    local tmpsplit = tmppath:split("/")
+    table.remove(tmpsplit, 1)
+    local parpath = protocol .. ":///" .. (table.concat(tmpsplit, "/"))
+
     table.insert(data, {
-      path = p1,
+      path = parpath,
       filename = "..",
       size = 0,
       type = "dir",
