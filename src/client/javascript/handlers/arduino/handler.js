c126e6b72f264ed45801f00c21a4c67db6147ffe
Anders Evenrud <andersevenrud@gmail.com>
2016-02-25 10:35:50 +0100

    Workaround for incognito browser bug

--- src/client/javascript/handlers/arduino/handler.js
+++ src/client/javascript/handlers/arduino/handler.js
@@ -109,6 +109,10 @@
     var opts = {username: username, password: password};
     this.callAPI('login', opts, function(response) {
       if ( response.result ) { // This contains an object with user data
+        try {
+          document.cookie = 'osjsuser=' + username;
+        } catch (e) {}
+
         callback(false, {
           userData: response.result.userData,
           userSettings: checkSettingsCompability(response.result.userSettings)
