c2a3c30c694877460807eba866324d71ee335ed7
Anders Evenrud <andersevenrud@gmail.com>
2016-04-02 21:03:51 +0200

    server-node: Added 'shadow' login handler
    Supports reading the shadow file to log in on *NIX type systems

---
+++ src/client/javascript/handlers/shadow/handler.js
@@ -0,0 +1,96 @@
+/*!
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Shadow Handler: Login screen and session/settings handling via database
+ * PLEASE NOTE THAT THIS AN EXAMPLE ONLY, AND SHOUD BE MODIFIED BEFORE USAGE
+ *
+ * Copyright (c) 2011-2016, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+//
+// See doc/pam-handler.txt
+//
+
+(function(API, Utils, VFS) {
+  'use strict';
+
+  window.OSjs  = window.OSjs || {};
+  OSjs.Core    = OSjs.Core   || {};
+
+  /////////////////////////////////////////////////////////////////////////////
+  // HANDLER
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @extends OSjs.Core._Handler
+   * @class
+   */
+  function ShadowHandler() {
+    OSjs.Core._Handler.apply(this, arguments);
+  }
+
+  ShadowHandler.prototype = Object.create(OSjs.Core._Handler.prototype);
+  ShadowHandler.constructor = OSjs.Core._Handler;
+
+  /**
+   * Override default init() method
+   */
+  ShadowHandler.prototype.init = function(callback) {
+    var self = this;
+    return OSjs.Core._Handler.prototype.init.call(this, function() {
+      self.initLoginScreen(callback);
+    });
+  };
+
+  /**
+   * Shadow login api call
+   */
+  ShadowHandler.prototype.login = function(username, password, callback) {
+    return OSjs.Core._Handler.prototype.login.apply(this, arguments);
+  };
+
+  /**
+   * Shadow logout api call
+   */
+  ShadowHandler.prototype.logout = function(save, callback) {
+    return OSjs.Core._Handler.prototype.logout.apply(this, arguments);
+  };
+
+  /**
+   * Shadow settings api call
+   */
+  ShadowHandler.prototype.saveSettings = function(pool, storage, callback) {
+    return OSjs.Core._Handler.prototype.saveSettings.apply(this, arguments);
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Core.Handler = ShadowHandler;
+
+})(OSjs.API, OSjs.Utils, OSjs.VFS);
