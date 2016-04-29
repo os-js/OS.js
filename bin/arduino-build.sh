44c89f672aa231d3a7de248892c5c307dbaf1bd3
Anders Evenrud <andersevenrud@gmail.com>
2016-04-29 16:49:03 +0200

    Updated build script

--- bin/arduino-build.sh
+++ bin/arduino-build.sh
@@ -120,6 +120,7 @@ rm $OUTDIR/dist/themes/.gitignore 2>/dev/null
 rm $OUTDIR/dist/packages/.gitignore 2>/dev/null
 rm $OUTDIR/dist/api.php 2>/dev/null
 rm $OUTDIR/dist/packages/*/*/package.json 2>/dev/null
+rm $OUTDIR/dist/packages/*/*/metadata.json 2>/dev/null
 rm $OUTDIR/dist/packages/*/*/api.js 2>/dev/null
 rm $OUTDIR/dist/packages/*/*/api.php 2>/dev/null
 rm $OUTDIR/dist/packages/*/*/server.lua 2>/dev/null
