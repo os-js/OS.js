3f6c2129daf69460303961d029574d4f556b6407
sergiotomasello <sergiotomasello@users.noreply.github.com>
2016-05-30 18:10:35 +0200

    update vers. number to 2.0.3

--- bin/arduino-build.sh
+++ bin/arduino-build.sh
@@ -169,8 +169,8 @@ if [ "$1" == "package" ]; then
     echo ""
 
     cd $OUTDIR
-    mv release arduinoos-2.0.1-$gitcommit
-    tar zcf arduinoos-2.0.1-"$gitcommit".tar.gz arduinoos-2.0.1-$gitcommit
+    mv release arduinoos-2.0.3-$gitcommit
+    tar zcf arduinoos-2.0.3-"$gitcommit".tar.gz arduinoos-2.0.3-$gitcommit
 fi
 
 rm -rf $TMPDIR
