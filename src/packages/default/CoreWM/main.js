f9386fda8b471fd5978a93251ef1c3f80cdafb1f
Anders Evenrud <andersevenrud@gmail.com>
2016-07-18 17:18:21 +0200

    CoreWM: Now using internal fullscreen API

--- src/packages/default/CoreWM/main.js
+++ src/packages/default/CoreWM/main.js
@@ -157,26 +157,10 @@
       }
 
       function toggleFullscreen() {
+        var docElm = document.documentElement;
         var notif = self.getNotificationIcon('_FullscreenNotification');
         if ( notif ) {
-          if ( notif.opts._isFullscreen ) {
-            if ( document.webkitCancelFullScreen ) {
-              document.webkitCancelFullScreen();
-            } else if ( document.mozCancelFullScreen ) {
-              document.mozCancelFullScreen();
-            } else if ( document.exitFullscreen ) {
-              document.exitFullscreen();
-            }
-          } else {
-            var docElm = document.documentElement;
-            if ( docElm.requestFullscreen ) {
-              docElm.requestFullscreen();
-            } else if ( docElm.mozRequestFullScreen ) {
-              docElm.mozRequestFullScreen();
-            } else if ( docElm.webkitRequestFullScreen ) {
-              docElm.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
-          }
+          API.toggleFullscreen(notif.opts._isFullscreen ? document : docElm, !notif.opts._isFullscreen);
         }
       }
 
