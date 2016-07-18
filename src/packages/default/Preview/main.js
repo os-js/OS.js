43b18e3e9acbe5f4671c0b3682cd3a761b3f6ebb
Anders Evenrud <andersevenrud@gmail.com>
2016-07-18 18:04:36 +0200

    Preview: Added zooming capabilities to image previews

--- src/packages/default/Preview/main.js
+++ src/packages/default/Preview/main.js
@@ -42,16 +42,43 @@
       width: 400,
       height: 200
     }, app, scheme, file]);
+
+    this.zoomLevel = 0;
+    this.isImage = true;
+    this.origWidth = 0;
+    this.origHeight = 0;
+    this.$view = null;
   }
 
   ApplicationPreviewWindow.prototype = Object.create(DefaultApplicationWindow.prototype);
   ApplicationPreviewWindow.constructor = DefaultApplicationWindow.prototype;
 
+  ApplicationPreviewWindow.prototype.destroy = function() {
+    this.$view = null;
+
+    return DefaultApplicationWindow.prototype.destroy.apply(this, arguments);
+  };
+
   ApplicationPreviewWindow.prototype.init = function(wm, app, scheme) {
+    var self = this;
     var root = DefaultApplicationWindow.prototype.init.apply(this, arguments);
 
     // Load and set up scheme (GUI) here
     scheme.render(this, 'PreviewWindow', root);
+
+    this._find('ZoomIn').son('click', this, this.onZoomIn);
+    this._find('ZoomOut').son('click', this, this.onZoomOut);
+    this._find('ZoomFit').son('click', this, this.onZoomFit);
+    this._find('ZoomOriginal').son('click', this, this.onZoomOriginal);
+
+    var c = this._find('Content').$element;
+    Utils.$bind(c, 'mousewheel', function(ev, pos) {
+      if ( pos.z === 1 ) {
+        self.onZoomOut();
+      } else if ( pos.z === -1 ) {
+        self.onZoomIn();
+      }
+    });
 
     return root;
   };
@@ -62,14 +89,20 @@
     Utils.$empty(root);
 
     if ( result ) {
+      this.zoomLevel = 0;
+
       if ( file.mime.match(/^image/) ) {
-        this._scheme.create(self, 'gui-image', {src: result}, root, {
+        this.isImage = true;
+        this.$view = this._scheme.create(self, 'gui-image', {src: result}, root, {
           onload: function() {
+            self.origWidth = this.offsetWidth;
+            self.origHeight = this.offsetHeight;
             self._resizeTo(this.offsetWidth, this.offsetHeight, true, false, this);
           }
         });
       } else if ( file.mime.match(/^video/) ) {
-        this._scheme.create(self, 'gui-video', {src: result, controls: true, autoplay: true}, root, {
+        this.isImage = false;
+        this.$view = this._scheme.create(self, 'gui-video', {src: result, controls: true, autoplay: true}, root, {
           onload: function() {
             self._resizeTo(this.offsetWidth, this.offsetHeight, true, false, this);
           }
@@ -77,7 +110,68 @@
       }
     }
 
+    var toolbar = this._find('Toolbar');
+    if ( toolbar ) {
+      toolbar[this.isImage ? 'show' : 'hide']();
+    }
+
     DefaultApplicationWindow.prototype.showFile.apply(this, arguments);
+  };
+
+  ApplicationPreviewWindow.prototype._onZoom = function(val) {
+    if ( !this.isImage || !this.$view ) {
+      return;
+    }
+
+    var zoom = ['in', 'out'].indexOf(val) !== -1;
+    var attr = 'zoomed';
+    var w = null;
+
+    if ( val === 'in' ) {
+      this.zoomLevel = Math.min(10, this.zoomLevel + 1);
+    } else if ( val === 'out' ) {
+      this.zoomLevel = Math.max(-10, this.zoomLevel - 1);
+    } else {
+      this.zoomLevel = 0;
+      attr = val === 'fit' ? val : '';
+    }
+
+    if ( zoom ) {
+      var z = this.zoomLevel;
+      if ( z === 0 ) {
+        z = 1;
+        w = this.origWidth;
+      } else if ( z > 0 ) {
+        z += 1;
+        w = this.origWidth * z;
+      } else if ( z < 0 ) {
+        z -= 1;
+        w = Math.abs(this.origWidth / z);
+      }
+
+      this._setTitle(this.currentFile.filename + ' (' + String(z * 100) + '%)', true);
+    } else {
+      this._setTitle(this.currentFile.filename, true);
+    }
+
+    this.$view.$element.setAttribute('data-zoom', attr);
+    this.$view.$element.firstChild.style.width = (w === null ? 'auto' : String(w) + 'px');
+  }
+
+  ApplicationPreviewWindow.prototype.onZoomIn = function() {
+    this._onZoom('in');
+  };
+
+  ApplicationPreviewWindow.prototype.onZoomOut = function() {
+    this._onZoom('out');
+  };
+
+  ApplicationPreviewWindow.prototype.onZoomFit = function() {
+    this._onZoom('fit');
+  };
+
+  ApplicationPreviewWindow.prototype.onZoomOriginal = function() {
+    this._onZoom();
   };
 
   /////////////////////////////////////////////////////////////////////////////
