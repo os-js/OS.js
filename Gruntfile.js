e3d82fc45b4f91c83ff46535d90b499d9a86392e
Anders Evenrud <andersevenrud@gmail.com>
2016-12-27 19:31:01 +0100

    Cleaned up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,156 +27,124 @@
  * @author  Anders Evenrud <andersevenrud@gmail.com>
  * @licence Simplified BSD License
  */
-(function(_fs, _path, _build, _grunt) {
+module.exports = function(grunt) {
   'use strict';
 
-  /////////////////////////////////////////////////////////////////////////////
-  // GRUNT
-  /////////////////////////////////////////////////////////////////////////////
+  grunt.file.defaultEncoding = 'utf-8';
 
-  module.exports = function(grunt) {
+  //grunt.loadNpmTasks('grunt-nw-builder');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-validate-xml');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.file.defaultEncoding = 'utf-8';
+  grunt.registerTask('test', ['eslint', 'csslint', 'validate_xml', 'mochaTest'/*, 'mocha'*/]);
 
-    //
-    // Load plugins
-    //
-    if ( grunt.option('nw') ) {
-      grunt.loadNpmTasks('grunt-nw-builder');
-    }
-
-    // Make sure we only load required modules (ignore warnings)
-    var checks = ['test', 'eslint', 'csslint', 'validate_xml', 'mochaTest', 'mocha'];
-    checks.forEach(function(k) {
-      if ( grunt.cli.tasks.indexOf(k) >= 0 ) {
-        grunt.loadNpmTasks('grunt-eslint');
-        grunt.loadNpmTasks('grunt-mocha-test');
-        grunt.loadNpmTasks('grunt-mocha');
-        grunt.loadNpmTasks('grunt-contrib-csslint');
-        grunt.loadNpmTasks('grunt-contrib-validate-xml');
-        return false;
-      }
-      return true;
-    });
-
-    if ( grunt.cli.tasks.indexOf('watch') >= 0 ) {
-      grunt.loadNpmTasks('grunt-contrib-watch');
-    }
-
-    //
-    // Load tasks
-    //
-    grunt.initConfig({
-      eslint: {
-        options: {
-          configFile: '.eslintrc'
-        },
-        target: [
-          'Gruntfile.js',
-          'src/*.js',
-          'src/build/*.js',
-          'src/server/node/*.js',
-          'src/server/node/**/*.js',
-          'src/client/javascript/*.js',
-          'src/client/javascript/**/*.js',
-          'src/packages/default/**/*.js',
-          '!src/packages/default/Broadway/**',
-          '!src/packages/default/**/locales.js',
-          '!src/packages/default/**/locale.js'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      options: {
+        configFile: '.eslintrc'
+      },
+      target: [
+        'Gruntfile.js',
+        'src/*.js',
+        'src/build/*.js',
+        'src/server/node/*.js',
+        'src/server/node/**/*.js',
+        'src/client/javascript/*.js',
+        'src/client/javascript/**/*.js',
+        'src/packages/default/**/*.js',
+        '!src/packages/default/**/locales.js',
+        '!src/packages/default/**/locale.js'
+      ]
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      strict: {
+        src: [
+          'src/client/stylesheets/*.css',
+          '!src/client/stylesheets/gui.css',
+          '!src/client/stylesheets/debug.css',
+          'src/client/themes/fonts/*/*.css',
+          'src/client/themes/styles/*/*.css',
+          'src/packages/default/*/*.css',
+          '!src/packages/default/CoreWM/animations.css'
         ]
       },
-      csslint: {
+      lax: {
         options: {
-          csslintrc: '.csslintrc'
+          'known-properties': false,
+          'compatible-vendor-prefixes': false
         },
-        strict: {
-          src: [
-            'src/client/stylesheets/*.css',
-            '!src/client/stylesheets/gui.css',
-            '!src/client/stylesheets/debug.css',
-            'src/client/themes/fonts/*/*.css',
-            'src/client/themes/styles/*/*.css',
-            'src/packages/default/*/*.css',
-            '!src/packages/default/CoreWM/animations.css'
-          ]
-        },
-        lax: {
-          options: {
-            'known-properties': false,
-            'compatible-vendor-prefixes': false
-          },
-          src: [
-            'src/client/stylesheets/gui.css',
-            'src/client/stylesheets/debug.css'
-          ]
-        }
-      },
-      mochaTest: {
-        test: {
-          src: ['src/server/test/node/*.js']
-        }
-      },
-      mocha: {
-        test: {
-          options: {
-            urls: [
-              'http://localhost:8000/test.html'
-            ]
-          }
-        }
-      },
-      watch: {
-        core: {
-          files: [
-            'src/client/stylesheets/*.css',
-            'src/client/javascript/*.js',
-            'src/client/javascript/*/*.js'
-          ],
-          tasks: ['build:core']
-        },
-        themes: {
-          files: [
-            'src/client/stylesheets/*.less',
-            'src/client/themes/styles/*/*.less',
-            'src/client/themes/fonts/*/*.css'
-          ],
-          tasks: ['build:themes']
-        },
-        configs: {
-          files: ['src/conf/*.json'],
-          tasks: ['build:config', 'build:core']
-        },
-        metadata: {
-          files: [
-            'src/client/themes/styles/*/metadata.json',
-            'src/client/themes/sounds/*/metadata.json',
-            'src/client/themes/icons/*/metadata.json',
-            'src/packages/*/*/metadata.json'
-          ],
-          tasks: ['build:config', 'build:manifest']
-        }
-      },
-      validate_xml: {
-        all: {
-          src: [
-            'src/client/dialogs.html',
-            'src/packages/default/*/scheme.html'
-          ]
-        }
-      },
-      nwjs: {
-        options: {
-          version: '0.12.3',
-          //version: '0.13.0-beta2',
-          //platforms: ['win', 'linux', 'osx'],
-          platforms: ['win64', 'linux64'],
-          buildDir: '.nw'
-        },
-        src: ['src/templates/nw/package.json', '.standalone/**/*']
+        src: [
+          'src/client/stylesheets/gui.css',
+          'src/client/stylesheets/debug.css'
+        ]
       }
-    });
-
-    grunt.registerTask('test', ['eslint', 'csslint', 'validate_xml', 'mochaTest'/*, 'mocha'*/]);
-  };
-
-})(require('node-fs-extra'), require('path'), require('./src/build/index.js'), require('grunt'));
+    },
+    mochaTest: {
+      test: {
+        src: ['src/server/test/node/*.js']
+      }
+    },
+    mocha: {
+      test: {
+        options: {
+          urls: ['http://localhost:8000/test.html']
+        }
+      }
+    },
+    watch: {
+      core: {
+        files: [
+          'src/client/stylesheets/*.css',
+          'src/client/javascript/*.js',
+          'src/client/javascript/*/*.js'
+        ],
+        tasks: ['build:core']
+      },
+      themes: {
+        files: [
+          'src/client/stylesheets/*.less',
+          'src/client/themes/styles/*/*.less',
+          'src/client/themes/fonts/*/*.css'
+        ],
+        tasks: ['build:themes']
+      },
+      configs: {
+        files: ['src/conf/*.json'],
+        tasks: ['build:config', 'build:core']
+      },
+      metadata: {
+        files: [
+          'src/client/themes/styles/*/metadata.json',
+          'src/client/themes/sounds/*/metadata.json',
+          'src/client/themes/icons/*/metadata.json',
+          'src/packages/*/*/metadata.json'
+        ],
+        tasks: ['build:config', 'build:manifest']
+      }
+    },
+    validate_xml: {
+      all: {
+        src: [
+          'src/client/dialogs.html',
+          'src/packages/default/*/scheme.html'
+        ]
+      }
+    },
+    nwjs: {
+      options: {
+        version: '0.12.3',
+        platforms: ['win64', 'linux64'],
+        buildDir: '.nw'
+      },
+      src: ['src/templates/nw/package.json', '.standalone/**/*']
+    }
+  });
+};
