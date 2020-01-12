4d8d949293c1cfdd3925a8350f9e1b70a7acb16d
Anders Evenrud <andersevenrud@gmail.com>
2020-01-12 03:37:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+# OS.js v3 official demo Docker image
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) desktop implementation for your browser with a fully-fledged window manager, Application APIs, GUI toolkits and filesystem abstraction.
+
+[![Support](https://img.shields.io/badge/patreon-support-orange.svg)](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)
+[![Back](https://opencollective.com/osjs/tiers/backer/badge.svg?label=backer&color=brightgreen)](https://opencollective.com/osjs)
+[![Sponsor](https://opencollective.com/osjs/tiers/sponsor/badge.svg?label=sponsor&color=brightgreen)](https://opencollective.com/osjs)
+[![Donate](https://img.shields.io/badge/liberapay-donate-yellowgreen.svg)](https://liberapay.com/os-js/)
+[![Donate](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://paypal.me/andersevenrud)
+[![Community](https://img.shields.io/badge/join-community-green.svg)](https://community.os-js.org/)
+
+## Usage
+
+This is just a basic demo of OS.js v3 using the base distro repository.
+
+```
+docker run -p 8000:8000 osjs/osjs:v3
+```
+
+* https://hub.docker.com/r/osjs/osjs
+* https://github.com/os-js/OS.js/tree/v3-demo
+
+## Contribution
+
+* **Become a [Patreon](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)**
+* **Support on [Open Collective](https://opencollective.com/osjs)**
+* [Contribution Guide](https://github.com/os-js/OS.js/blob/v3/CONTRIBUTING.md)
+
+## Documentation
+
+See the [Official Manuals](https://manual.os-js.org/v3/) for articles, tutorials and guides.
+
+## Links
+
+* [Official Chat](https://gitter.im/os-js/OS.js)
+* [Community Forums and Announcements](https://community.os-js.org/)
+* [Homepage](https://os-js.org/)
+* [Twitter](https://twitter.com/osjsorg) ([author](https://twitter.com/andersevenrud))
+* [Google+](https://plus.google.com/b/113399210633478618934/113399210633478618934)
+* [Facebook](https://www.facebook.com/os.js.org)
+* [Docker Hub](https://hub.docker.com/u/osjs/)
---
+++ src/server/index.js
@@ -0,0 +1,69 @@
+/*
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) 2011-2020, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+//
+// This is the server bootstrapping script.
+// This is where you can register service providers or set up
+// your libraries etc.
+//
+// https://manual.os-js.org/v3/guide/provider/
+// https://manual.os-js.org/v3/install/
+// https://manual.os-js.org/v3/resource/official/
+//
+
+const {
+  Core,
+  CoreServiceProvider,
+  PackageServiceProvider,
+  VFSServiceProvider,
+  AuthServiceProvider,
+  SettingsServiceProvider
+} = require('@osjs/server');
+
+const config = require('./config.js');
+const osjs = new Core(config, {});
+
+osjs.register(CoreServiceProvider, {before: true});
+osjs.register(PackageServiceProvider);
+osjs.register(VFSServiceProvider);
+osjs.register(AuthServiceProvider);
+osjs.register(SettingsServiceProvider);
+
+process.on('SIGTERM', () => osjs.destroy());
+process.on('SIGINT', () => osjs.destroy());
+process.on('exit', () => osjs.destroy());
+process.on('uncaughtException', e => console.error(e));
+process.on('unhandledRejection', e => console.error(e));
+
+osjs.boot()
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
